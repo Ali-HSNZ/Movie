20dbba94d1b0455c505cbce3b9ad9c2f16737a5e
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-16 20:55:09 +0330

    Add layout

---
+++ src/Layout/Header/Header.jsx
@@ -0,0 +1,9 @@
+import Navigation from "../../Components/Navigation/Navigation";
+
+const Header = () => {
+    return (  
+        <Navigation/>
+    );
+}
+ 
+export default Header;
---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import Header from './Header/Header';
+import Styles from './Layout.module.css'
+const Layout = ({children}) => {
+    return ( 
+        <div className={Styles.parent}>
+            <div className={Styles.main}>
+                <Header/>
+                {children}
+            </div>
+        </div>
+    );
+}
+ 
+export default Layout;
