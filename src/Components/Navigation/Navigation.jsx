994433cebc472018691faacd4fb159268301153d
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-16 20:56:51 +0330

    add Navigation

---
+++ src/Components/Navigation/Navigation.jsx
@@ -0,0 +1,11 @@
+import Styles from './Navigation.module.css'
+const Navigation = () => {
+    return (  
+       
+        <div className={Styles.parent}>
+            
+        </div>
+    );
+}
+ 
+export default Navigation;
