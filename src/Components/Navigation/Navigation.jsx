87e29c4b19cfccaf6fe666b5059ee7737089fea5
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-16 23:30:18 +0330

    Create Navigation

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,9 +1,30 @@
+import { NavLink } from 'react-router-dom';
 import Styles from './Navigation.module.css'
+import { BiSearch } from "react-icons/bi";
 const Navigation = () => {
     return (  
        
         <div className={Styles.parent}>
-            
+
+            <div className={Styles.left}>
+
+                <div className={Styles.logoParent}>
+                    <div>IMDB</div>    
+                </div>
+
+                <div className={Styles.searchParent}>
+                    <input type='text' placeholder='Search The Movie Name'/>
+                    <BiSearch className={Styles.inputSearchIcon} size='23px'/>
+                    <button>Advanced</button>    
+                </div>     
+            </div>
+
+            <nav className={Styles.right}>
+                <NavLink to={{pathname:"/3"}} className={ ({isActive})=> isActive ? Styles.navLink : Styles.navLinkActive}>Coming Soon</NavLink>    
+                <NavLink to={{pathname:"/"}} className={ ({isActive})=> isActive ? Styles.navLink : Styles.navLinkActive}>Popular Series</NavLink>    
+                <NavLink to={{pathname:"/4"}} className={ ({isActive})=> isActive ? Styles.navLink : Styles.navLinkActive}>Popular Movies</NavLink>    
+            </nav>
+           
         </div>
     );
 }
