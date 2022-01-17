b47a2fd7d6e2e09900f103c5a2e3ee3880230569
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-18 00:40:04 +0330

    render TopMoviesRating Component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import BestMovieMiniSlider from "../../Components/Best Movie Mini Slider/BestMovieMiniSlider";
 import BestSerialMiniSlider from "../../Components/Best Serial Mini Slider/BestSerialMiniSlider";
 import SliderGenreBanner from "../../Components/Slider & Genre Banner/SliderGenreBanner";
+import TopMoviesRating from "../../Components/Top Movies Rating/TopMoviesRating";
 
 const Home = () => {
     return ( 
@@ -8,6 +9,7 @@ const Home = () => {
             <SliderGenreBanner/>
             <BestSerialMiniSlider/>
             <BestMovieMiniSlider/>
+            <TopMoviesRating/>
         </>
         
     );
