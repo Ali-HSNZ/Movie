17ef4c3e04a3c756ee2bfd5fef13f0e689545fc8
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-17 19:10:32 +0330

    render BestSerialMiniSlider Component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,13 @@
+import BestSerialMiniSlider from "../../Components/Best Serial Mini Slider/BestSerialMiniSlider";
 import SliderGenreBanner from "../../Components/Slider & Genre Banner/SliderGenreBanner";
 
 const Home = () => {
     return ( 
-        <SliderGenreBanner/>
+        <>
+            <SliderGenreBanner/>
+            <BestSerialMiniSlider/>
+        </>
+        
     );
 }
  
