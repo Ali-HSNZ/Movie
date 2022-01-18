bb53d1434b6513346816738e086d29416e100c15
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-18 17:26:17 +0330

    create BestAnimationMiniSlider Component

---
+++ src/Components/Best Animation Mini Slider/BestAnimationMiniSlider.jsx
@@ -0,0 +1,84 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import '../Mini Slider Style/MiniSlider.css'
+import Styles from '../Mini Slider Style/MiniSlider.module.css'
+
+import { AiFillCaretRight } from "react-icons/ai";
+
+// Slider ==>
+
+import "swiper/css";
+import "swiper/css/free-mode"
+
+import SwiperCore, {
+  FreeMode,Navigation
+} from 'swiper';
+
+// <==  Slider 
+
+SwiperCore.use([FreeMode , Navigation]);
+
+
+
+const BestAnimationMiniSlider = () => {
+    return (  
+        <div className="slider_miniSlider">
+            <div className={Styles.silderTitle}>
+                <a href="/">The best Animation Movie in 2021</a>
+                <AiFillCaretRight/>
+            </div>
+            <Swiper slidesPerView={6} spaceBetween={10} navigation freeMode={true}>
+               
+                <SwiperSlide className={Styles.sliderSlideParent}>
+                    <img className={Styles.sliderSlide} src="https://m.media-amazon.com/images/M/MV5BNjE5NzA4ZDctOTJkZi00NzM0LTkwOTYtMDI4MmNkMzIxODhkXkEyXkFqcGdeQXVyNjY1MTg4Mzc@._V1_.jpg"/>
+                </SwiperSlide>
+                
+                <SwiperSlide  className={Styles.sliderSlideParent}>
+                    <img className={Styles.sliderSlide} src="https://m.media-amazon.com/images/M/MV5BMWRiZGQ1NDMtODQ2OS00MDlhLWJkZGYtM2ZmNjlhZThjOWRmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg"/>
+                </SwiperSlide>
+                
+                <SwiperSlide  className={Styles.sliderSlideParent}>
+                    <img className={Styles.sliderSlide} src="https://m.media-amazon.com/images/M/MV5BZWM4MzUyYTAtMGFiYy00MmRkLWIyNDktMWFlMDlmZjJmNWZmXkEyXkFqcGdeQXVyMTM1MTE1NDMx._V1_.jpg"/>
+                </SwiperSlide>  
+
+                <SwiperSlide  className={Styles.sliderSlideParent}>
+                    <img className={Styles.sliderSlide} src="https://m.media-amazon.com/images/M/MV5BMTYzODYzODU2Ml5BMl5BanBnXkFtZTgwNTc1MTA2NzE@._V1_.jpg"/>
+                </SwiperSlide>  
+
+                <SwiperSlide  className={Styles.sliderSlideParent}>
+                    <img className={Styles.sliderSlide} src="https://m.media-amazon.com/images/M/MV5BMjMwNDkxMTgzOF5BMl5BanBnXkFtZTgwNTkwNTQ3NjM@._V1_.jpg"/>
+                </SwiperSlide>    
+
+                <SwiperSlide  className={Styles.sliderSlideParent}>
+                    <img className={Styles.sliderSlide} src="https://m.media-amazon.com/images/M/MV5BYzFhNDNkY2UtYjc3ZS00NzVkLTlhNzEtYmZiZGMzYmRjMjVhXkEyXkFqcGdeQXVyMjQwMDg0Ng@@._V1_.jpg"/>
+                </SwiperSlide>   
+
+                <SwiperSlide  className={Styles.sliderSlideParent}>
+                    <img className={Styles.sliderSlide} src="https://m.media-amazon.com/images/M/MV5BOGU2Yjc0Y2YtMDU0MS00NTFiLTlkMGMtOTcxOWIzMjhkZmZkXkEyXkFqcGdeQXVyMDA4NzMyOA@@._V1_.jpg"/>
+                </SwiperSlide>
+
+                <SwiperSlide  className={Styles.sliderSlideParent}>
+                    <img className={Styles.sliderSlide} src="https://m.media-amazon.com/images/M/MV5BMzMxMjllYTgtZWNjZS00ODc0LTljZWUtYjdiMDQ2ZDJkM2FkXkEyXkFqcGdeQXVyNjY1MTg4Mzc@._V1_.jpg"/>
+                </SwiperSlide>
+
+
+                <SwiperSlide  className={Styles.sliderSlideParent}>
+                    <img className={Styles.sliderSlide} src="https://m.media-amazon.com/images/M/MV5BZWNiOTc4NGItNGY4YS00ZGNkLThkOWEtMDE2ODcxODEwNjkwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg"/>
+                </SwiperSlide>
+
+
+                <SwiperSlide  className={Styles.sliderSlideParent}>
+                    <img className={Styles.sliderSlide} src="https://m.media-amazon.com/images/M/MV5BMTM3NjQyODI3M15BMl5BanBnXkFtZTcwMDM4NjM0OA@@._V1_.jpg"/>
+                </SwiperSlide>
+
+
+                <SwiperSlide  className={Styles.sliderSlideParent}>
+                    <img className={Styles.sliderSlide} src="https://m.media-amazon.com/images/M/MV5BZTQyNTU0MDktYTFkYi00ZjNhLWE2ODctMzBkM2U1ZTk3YTMzXkEyXkFqcGdeQXVyNTI4MzE4MDU@._V1_.jpg"/>
+                </SwiperSlide>
+                
+            </Swiper>
+        </div>
+    );
+}
+ 
+export default BestAnimationMiniSlider;
