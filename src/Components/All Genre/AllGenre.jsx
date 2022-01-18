1b5270d833e0f1d8d942d72a2b54be870974f953
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-18 13:43:51 +0330

    Create AllGenre Component

---
+++ src/Components/All Genre/AllGenre.jsx
@@ -0,0 +1,162 @@
+import Styles from "./AllGenre.module.css"
+import { AiFillCaretRight } from "react-icons/ai";
+
+const AllGenre = () => {
+    return (  
+        <div className={Styles.parent}>
+            <div className={Styles.header}>
+                <p className={Styles.headerTitle}>
+                    All genres
+                    <AiFillCaretRight/>
+                </p>
+            </div>
+            <div className={Styles.footer}>
+
+                <div className={Styles.row}>
+                    <div className={Styles.rowItem}>
+                        <div className={Styles.genre}>
+                                <section className={Styles.genreRanke_genreLine}>
+                                    <p>1</p>
+                                    <section className={Styles.genreLine}></section>
+                                </section>
+                                
+                                <p className={Styles.genreTitle}>Adventure</p>
+                                <button className={Styles.genreSubmit}>Go</button>
+                        </div>
+                        <span className={Styles.genreVidoeCount}>Video : 15</span>
+                    </div>
+
+                    <div className={Styles.rowItem}>
+                        <div className={Styles.genre}>
+                                <section className={Styles.genreRanke_genreLine}>
+                                    <p>2</p>
+                                    <section className={Styles.genreLine}></section>
+                                </section>
+                                
+                                <p className={Styles.genreTitle}>Adventure</p>
+                                <button className={Styles.genreSubmit}>Go</button>
+                        </div>
+                        <span className={Styles.genreVidoeCount}>Video : 15</span>
+                    </div>
+                    
+                    <div className={Styles.rowItem}>
+                        <div className={Styles.genre}>
+                                <section className={Styles.genreRanke_genreLine}>
+                                    <p>3</p>
+                                    <section className={Styles.genreLine}></section>
+                                </section>
+                                
+                                <p className={Styles.genreTitle}>Adventure</p>
+                                <button className={Styles.genreSubmit}>Go</button>
+                        </div>
+                        <span className={Styles.genreVidoeCount}>Video : 15</span>
+                    </div>
+
+
+
+
+
+                </div>
+
+                <div className={Styles.row}>
+                    <div className={Styles.rowItem}>
+                        <div className={Styles.genre}>
+                                <section className={Styles.genreRanke_genreLine}>
+                                    <p>4</p>
+                                    <section className={Styles.genreLine}></section>
+                                </section>
+                                
+                                <p className={Styles.genreTitle}>Adventure</p>
+                                <button className={Styles.genreSubmit}>Go</button>
+                        </div>
+                        <span className={Styles.genreVidoeCount}>Video : 15</span>
+                    </div>
+
+                    <div className={Styles.rowItem}>
+                        <div className={Styles.genre}>
+                                <section className={Styles.genreRanke_genreLine}>
+                                    <p>5</p>
+                                    <section className={Styles.genreLine}></section>
+                                </section>
+                                
+                                <p className={Styles.genreTitle}>Adventure</p>
+                                <button className={Styles.genreSubmit}>Go</button>
+                        </div>
+                        <span className={Styles.genreVidoeCount}>Video : 15</span>
+                    </div>
+                    
+                    <div className={Styles.rowItem}>
+                        <div className={Styles.genre}>
+                                <section className={Styles.genreRanke_genreLine}>
+                                    <p>6</p>
+                                    <section className={Styles.genreLine}></section>
+                                </section>
+                                
+                                <p className={Styles.genreTitle}>Adventure</p>
+                                <button className={Styles.genreSubmit}>Go</button>
+                        </div>
+                        <span className={Styles.genreVidoeCount}>Video : 15</span>
+                    </div>
+
+
+
+
+
+                </div>
+
+                <div className={Styles.row}>
+                    <div className={Styles.rowItem}>
+                        <div className={Styles.genre}>
+                                <section className={Styles.genreRanke_genreLine}>
+                                    <p>7</p>
+                                    <section className={Styles.genreLine}></section>
+                                </section>
+                                
+                                <p className={Styles.genreTitle}>Adventure</p>
+                                <button className={Styles.genreSubmit}>Go</button>
+                        </div>
+                        <span className={Styles.genreVidoeCount}>Video : 15</span>
+                    </div>
+
+                    <div className={Styles.rowItem}>
+                        <div className={Styles.genre}>
+                                <section className={Styles.genreRanke_genreLine}>
+                                    <p>8</p>
+                                    <section className={Styles.genreLine}></section>
+                                </section>
+                                
+                                <p className={Styles.genreTitle}>Adventure</p>
+                                <button className={Styles.genreSubmit}>Go</button>
+                        </div>
+                        <span className={Styles.genreVidoeCount}>Video : 15</span>
+                    </div>
+                    
+                    <div className={Styles.rowItem}>
+                        <div className={Styles.genre}>
+                                <section className={Styles.genreRanke_genreLine}>
+                                    <p>9</p>
+                                    <section className={Styles.genreLine}></section>
+                                </section>
+                                
+                                <p className={Styles.genreTitle}>Adventure</p>
+                                <button className={Styles.genreSubmit}>Go</button>
+                        </div>
+                        <span className={Styles.genreVidoeCount}>Video : 15</span>
+                    </div>
+
+
+
+
+
+                </div>
+
+            </div>
+
+            <div className={Styles.cover}>
+                <button>Load More</button>
+            </div>
+        </div>
+    );
+}
+ 
+export default AllGenre;
