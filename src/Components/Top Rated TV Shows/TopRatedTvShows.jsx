40e12f45e51e306a1da3cb0240cf4fb7ba3031e1
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-19 19:35:55 +0330

    Create TopRatedTvShows Component

---
+++ src/Components/Top Rated TV Shows/TopRatedTvShows.jsx
@@ -0,0 +1,84 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import '../Mini Slider Style/MiniSlider.css'
+import Styles from '../Mini Slider Style/MiniSlider.module.css'
+
+import { AiFillCaretRight } from "react-icons/ai";
+
+// Slider ==>
+
+import "swiper/css";
+import "swiper/css/free-mode"
+
+import SwiperCore, {
+  FreeMode,Navigation
+} from 'swiper';
+
+// <==  Slider 
+
+SwiperCore.use([FreeMode , Navigation]);
+
+
+
+const TopRatedTvShow = () => {
+    return (  
+        <div className="slider_miniSlider">
+            <div className={Styles.silderTitle}>
+                <a href="/">Top Rated Tv Show</a>
+                <AiFillCaretRight/>
+            </div>
+            <Swiper slidesPerView={6} spaceBetween={10} navigation freeMode={true}>
+               
+                <SwiperSlide className={Styles.sliderSlideParent}>
+                    <img className={Styles.sliderSlide} src='https://m.media-amazon.com/images/M/MV5BZWYxODViMGYtMGE2ZC00ZGQ3LThhMWUtYTVkNGE3OWU4NWRkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMjYwNDA2MDE@._V1_.jpg'/>
+                </SwiperSlide>
+                
+                <SwiperSlide  className={Styles.sliderSlideParent}>
+                    <img className={Styles.sliderSlide} src="https://m.media-amazon.com/images/M/MV5BNmZlYzIzMTItY2EzYS00YTEyLTg0ZjEtMDMzZjM3ODdhN2UzXkEyXkFqcGdeQXVyNjI0MDg2NzE@._V1_.jpg"/>
+                </SwiperSlide>
+                
+                <SwiperSlide  className={Styles.sliderSlideParent}>
+                    <img className={Styles.sliderSlide} src="https://m.media-amazon.com/images/M/MV5BOWNlMjBjZTUtNThiNy00OTkxLThiZTQtNTEyZDliZTM3N2Q0XkEyXkFqcGdeQXVyMTMzNDExODE5._V1_.jpg"/>
+                </SwiperSlide>  
+
+                <SwiperSlide  className={Styles.sliderSlideParent}>
+                    <img className={Styles.sliderSlide} src="https://m.media-amazon.com/images/M/MV5BMTI3ODc2ODc0M15BMl5BanBnXkFtZTYwMjgzNjc3._V1_.jpg"/>
+                </SwiperSlide>  
+
+                <SwiperSlide  className={Styles.sliderSlideParent}>
+                    <img className={Styles.sliderSlide} src="https://m.media-amazon.com/images/M/MV5BZGQ2YmMxZmEtYjI5OS00NzlkLTlkNTEtYWMyMzkyMzc2MDU5XkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_.jpg"/>
+                </SwiperSlide>    
+
+                <SwiperSlide  className={Styles.sliderSlideParent}>
+                    <img className={Styles.sliderSlide} src="https://m.media-amazon.com/images/M/MV5BNTllYzFhMjAtZjExNS00MjM4LWE5YmMtOGFiZGRlOTU5YzJiXkEyXkFqcGdeQXVyNDIzMzcwNjc@._V1_.jpg"/>
+                </SwiperSlide>   
+
+                <SwiperSlide  className={Styles.sliderSlideParent}>
+                    <img className={Styles.sliderSlide} src="https://m.media-amazon.com/images/M/MV5BYjg2ODk0MjUtNmMzZS00MjY0LWI1YWMtN2JhMjRmZGUwY2I3XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"/>
+                </SwiperSlide>
+
+                <SwiperSlide  className={Styles.sliderSlideParent}>
+                    <img className={Styles.sliderSlide} src="https://m.media-amazon.com/images/M/MV5BN2I1ZjA5YjQtYmQ0ZS00ZmE1LTk1ZjktNTQ5ODIzY2JiZDdhXkEyXkFqcGdeQXVyNjg2NjQwMDQ@._V1_.jpg"/>
+                </SwiperSlide>
+
+
+                <SwiperSlide  className={Styles.sliderSlideParent}>
+                    <img className={Styles.sliderSlide} src="https://m.media-amazon.com/images/M/MV5BZTk5OTQyZjYtMDk3Yy00YjhmLWE2MTYtZmY4NTg1YWUzZTQ0XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg"/>
+                </SwiperSlide>
+
+
+                <SwiperSlide  className={Styles.sliderSlideParent}>
+                    <img className={Styles.sliderSlide} src="https://m.media-amazon.com/images/M/MV5BODc5YTBhMTItMjhkNi00ZTIxLWI0YjAtNTZmOTY0YjRlZGQ0XkEyXkFqcGdeQXVyODUwNjEzMzg@._V1_.jpg"/>
+                </SwiperSlide>
+
+
+                <SwiperSlide  className={Styles.sliderSlideParent}>
+                    <img className={Styles.sliderSlide} src="https://m.media-amazon.com/images/M/MV5BMTY4MGQyNjgtMzdmZS00MjQ5LWIyMzItYjYyZmQzNjVhYjMyXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg"/>
+                </SwiperSlide>
+                
+            </Swiper>
+        </div>
+    );
+}
+ 
+export default TopRatedTvShow;
