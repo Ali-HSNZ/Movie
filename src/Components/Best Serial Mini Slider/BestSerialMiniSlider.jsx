80a7c4141c118ec9e8dc16d6b868de2b95728125
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-21 22:03:26 +0330

    Fethcing data with api endPoint => BestSerialMiniSlider Component

--- src/Components/Best Serial Mini Slider/BestSerialMiniSlider.jsx
+++ src/Components/Best Serial Mini Slider/BestSerialMiniSlider.jsx
@@ -13,6 +13,8 @@ import "swiper/css/free-mode"
 import SwiperCore, {
   FreeMode,Navigation
 } from 'swiper';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 // <==  Slider 
 
@@ -21,63 +23,42 @@ SwiperCore.use([FreeMode , Navigation]);
 
 
 const BestSerialMiniSlider = () => {
+
+    const [data , setData] = useState(null)
+
+
+    // Fetching Top 250 Movies
+    useEffect(()=>{
+        const getData = async() =>{
+            // const endPoint = "https://imdb-api.com/en/API/MostPopularTVs/k_t4uhs4ca"
+            const endPoint = "https://imdb-api.com/en/API/MostPopularTVs/k_t4uhs4c"
+            await axios.get(endPoint)
+            .then(response => setData(response.data.items.slice(0,20)))
+            // .then(response => console.log(response.data))
+
+            .catch()
+        }
+        getData()
+    },[])
+
+    console.log(data)
+
     return (  
         <div className="slider_miniSlider">
             <div className={Styles.silderTitle}>
-                <a href="/">The best series in 2021</a>
+                <a href="/">Polular Series TVs</a>
                 <AiFillCaretRight/>
             </div>
-            <Swiper slidesPerView={6} spaceBetween={10} navigation freeMode={true}  className="mySwiper">
-               
-                <SwiperSlide className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BNzBhNDUzMmMtYzMyYi00NTJkLTg5OGYtYjAxMzMyNWM3YWE0XkEyXkFqcGdeQXVyMDM0MzU2NA@@._V1_.jpg"/>
-                </SwiperSlide>
-                
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BMWM2YWQ3NjItN2M4Yy00NjlmLWI5YmMtYzEyMjFjYzUxZTlkXkEyXkFqcGdeQXVyMjExMjk0ODk@._V1_.jpg"/>
-                </SwiperSlide>
-                
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BZjkxMmMzNzYtODM5ZS00ZGFkLWFlMjItMTA5MTE0ZDZmMzQ5L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjc2NjA5MTU@._V1_.jpg"/>
-                </SwiperSlide>  
 
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BYzM5OWU5YmMtYzQ0ZC00YWQ2LWFmMzUtNTA2YWI2Yzk0ZmMxXkEyXkFqcGdeQXVyMzAzNTY3MDM@._V1_.jpg"/>
-                </SwiperSlide>  
-
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BMTk3MzUwOTU2NF5BMl5BanBnXkFtZTcwOTk2MTgyMQ@@._V1_.jpg"/>
-                </SwiperSlide>    
-
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BYmM2YjFiMjUtYzY4NS00NjViLThkMTYtZDFkZGQyMDFlZTAyXkEyXkFqcGdeQXVyMTg2NjYzOA@@._V1_.jpg"/>
-                </SwiperSlide>   
-
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BZWNmNWNlZWMtYmRjMy00YzRiLWI3NDYtYzNlNDUzZDA0YjAwXkEyXkFqcGdeQXVyMTE2MTc3MzU1._V1_.jpg"/>
-                </SwiperSlide>
-
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BMzcxNmU1ZDAtZGViOC00Y2Q2LWJiODgtNzhlOGJjY2Q2MTM2XkEyXkFqcGdeQXVyMTA0MTI2ODE4._V1_.jpg"/>
-                </SwiperSlide>
-
-
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BMTc3NDYyNzYxM15BMl5BanBnXkFtZTgwMzgxMjEzNDM@._V1_.jpg"/>
-                </SwiperSlide>
-
-
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BYmU4ZjM2ZWMtMDFhMy00MGM4LTlkMmEtYTJhZTU5ODAzODk0XkEyXkFqcGdeQXVyNjc3MjQzNTI@._V1_.jpg"/>
-                </SwiperSlide>
-
-
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BMTY1NzIyNjM1MF5BMl5BanBnXkFtZTgwNjY2NDUxMjE@._V1_.jpg"/>
-                </SwiperSlide>
-
-
-
+            <Swiper slidesPerView={6} spaceBetween={10} navigation freeMode={true}>
+                {!data && <p style={{color:'#ffffff'}}>Loading...</p>}
+                {data &&  data.length > 0 && data.map(items => {
+                    return(
+                        <SwiperSlide  className={Styles.sliderSlideParent} key={items.id}>
+                            <img className={Styles.sliderSlide} src={`https://img.gs/knzwmsmxwd/268x215,quality=low/${items.image}`}/>
+                        </SwiperSlide>
+                    )
+                })}
             </Swiper>
         </div>
     );
