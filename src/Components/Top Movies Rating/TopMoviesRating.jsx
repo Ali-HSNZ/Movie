f14b113dc92595d96db039e1251b42ee555fa112
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-22 20:17:41 +0330

    Removed 'itemParent' className => TopMoviesRating Component

--- src/Components/Top Movies Rating/TopMoviesRating.jsx
+++ src/Components/Top Movies Rating/TopMoviesRating.jsx
@@ -12,133 +12,74 @@ const TopMoviesRating = () => {
 
             <div className={Styles.footer}>
 
-                <div className={Styles.right}>
 
-                    <div className={Styles.itemParent}>
-                        <div className={Styles.item}>
+                    <div className={Styles.item}>
 
-                            <div className={Styles.item_numberLine}>
-                                <p>1</p>
-                                <div className={Styles.item_line}></div>
-                            </div>
-                            
-                            <div className={Styles.desc}>
-                                <p className={Styles.item_movieTitle}>The Get Together</p>
-                                <p className={Styles.item_movieRating}>Rating : 9.4</p>
-                            </div>
-                            <div className={Styles.item_play}>
-                                <BsFillCaretRightFill size='24px'/>
-                            </div>
-
+                        <div className={Styles.item_numberLine}>
+                            <p>1</p>
+                            <div className={Styles.item_line}></div>
                         </div>
+                        
+                        <div className={Styles.desc}>
+                            <p className={Styles.item_movieTitle}>The Get Together</p>
+                            <p className={Styles.item_movieRating}>Rating : 9.4</p>
+                        </div>
+                        <div className={Styles.item_play}>
+                            <BsFillCaretRightFill size='24px'/>
+                        </div>
+
+                    </div>
+                    
+                    <div className={Styles.item}>
+
+                        <div className={Styles.item_numberLine}>
+                            <p>1</p>
+                            <div className={Styles.item_line}></div>
+                        </div>
+                        
+                        <div className={Styles.desc}>
+                            <p className={Styles.item_movieTitle}>The Get Together</p>
+                            <p className={Styles.item_movieRating}>Rating : 9.4</p>
+                        </div>
+                        <div className={Styles.item_play}>
+                            <BsFillCaretRightFill size='24px'/>
+                        </div>
+
                     </div>
 
-                    <div className={Styles.itemParent}>
-                        <div className={Styles.item}>
+                    <div className={Styles.item}>
 
-                            <div className={Styles.item_numberLine}>
-                                <p>2</p>
-                                <div className={Styles.item_line}></div>
-                            </div>
-
-                            <div className={Styles.desc}>
-                                <p className={Styles.item_movieTitle}>The Get Together</p>
-                                <p className={Styles.item_movieRating}>Rating : 9.4</p>
-                            </div>
-                            
-                            <div className={Styles.item_play}>
-                                <BsFillCaretRightFill size='24px'/>
-                            </div>
-
+                        <div className={Styles.item_numberLine}>
+                            <p>1</p>
+                            <div className={Styles.item_line}></div>
                         </div>
-                    </div>
-
-                    <div className={Styles.itemParent}>
-                        <div className={Styles.item}>
-
-                            <div className={Styles.item_numberLine}>
-                                <p>3</p>
-                                <div className={Styles.item_line}></div>
-                            </div>
-
-                            <div className={Styles.desc}>
-                                <p className={Styles.item_movieTitle}>The Get Together</p>
-                                <p className={Styles.item_movieRating}>Rating : 9.4</p>
-                            </div>
-                            
-                            <div className={Styles.item_play}>
-                                <BsFillCaretRightFill size='24px'/>
-                            </div>
-
+                        
+                        <div className={Styles.desc}>
+                            <p className={Styles.item_movieTitle}>The Get Together</p>
+                            <p className={Styles.item_movieRating}>Rating : 9.4</p>
                         </div>
-                    </div>
-
-                </div>
-
-                <div className={Styles.left}>
-
-                    <div className={Styles.itemParent}>
-                        <div className={Styles.item}>
-
-                            <div className={Styles.item_numberLine}>
-                                <p>4</p>
-                                <div className={Styles.item_line}></div>
-                            </div>
-
-                            <div className={Styles.desc}>
-                                <p className={Styles.item_movieTitle}>The Get Together</p>
-                                <p className={Styles.item_movieRating}>Rating : 9.4</p>
-                            </div>
-                            
-                            <div className={Styles.item_play}>
-                                <BsFillCaretRightFill size='24px'/>
-                            </div>
-
+                        <div className={Styles.item_play}>
+                            <BsFillCaretRightFill size='24px'/>
                         </div>
+
                     </div>
+                    
+                    <div className={Styles.item}>
 
-                    <div className={Styles.itemParent}>
-                        <div className={Styles.item}>
-
-                            <div className={Styles.item_numberLine}>
-                                <p>5</p>
-                                <div className={Styles.item_line}></div>
-                            </div>
-
-                            <div className={Styles.desc}>
-                                <p className={Styles.item_movieTitle}>The Get Together</p>
-                                <p className={Styles.item_movieRating}>Rating : 9.4</p>
-                            </div>
-                            
-                            <div className={Styles.item_play}>
-                                <BsFillCaretRightFill size='24px'/>
-                            </div>
-
+                        <div className={Styles.item_numberLine}>
+                            <p>1</p>
+                            <div className={Styles.item_line}></div>
                         </div>
-                    </div>
-
-                    <div className={Styles.itemParent}>
-                        <div className={Styles.item}>
-
-                            <div className={Styles.item_numberLine}>
-                                <p>6</p>
-                                <div className={Styles.item_line}></div>
-                            </div>
-
-                            <div className={Styles.desc}>
-                                <p className={Styles.item_movieTitle}>The Get Together</p>
-                                <p className={Styles.item_movieRating}>Rating : 9.4</p>
-                            </div>
-                            
-                            <div className={Styles.item_play}>
-                                <BsFillCaretRightFill size='24px'/>
-                            </div>
-
+                        
+                        <div className={Styles.desc}>
+                            <p className={Styles.item_movieTitle}>The Get Together</p>
+                            <p className={Styles.item_movieRating}>Rating : 9.4</p>
                         </div>
+                        <div className={Styles.item_play}>
+                            <BsFillCaretRightFill size='24px'/>
+                        </div>
+
                     </div>
-
-                </div>
-
             </div>
 
 
