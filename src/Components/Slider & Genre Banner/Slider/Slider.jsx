7d7bb645e40616193b41fd117ee2dc12780c3193
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-22 20:00:40 +0330

    Fetching Movie Data with API => Slider Component

--- src/Components/Slider & Genre Banner/Slider/Slider.jsx
+++ src/Components/Slider & Genre Banner/Slider/Slider.jsx
@@ -1,57 +1,68 @@
 import Styles from './Slider.module.css'
 
-import { Navigation} from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import './manageSlider.css'
+import { useEffect, useState } from 'react';
 
-
+import axios from 'axios'
 const Slider = () => {
+
+    const [dataMovie , setDataMovie] = useState({data : []})
+
+    useEffect(()=>{
+        const getAllImdbCode = async() => {
+            const endPoint = "https://data-imdb1.p.rapidapi.com/movie/order/byRating/"
+            await axios.get(endPoint ,{
+                params : { page_size: '1'}, 
+                headers: {
+                    'x-rapidapi-host': 'data-imdb1.p.rapidapi.com',
+                    'x-rapidapi-key': '61398a6ee8msh0033ca9207b7556p1fdb09jsn1ea8d45f729a'
+                },
+            })
+            .then(response => {
+                const  responseData =  response.data.results;
+                responseData.map(movieId => {
+                    const id = movieId.imdb_id
+
+                    axios.get(`https://imdb8.p.rapidapi.com/title/get-images`,{
+                        headers : {     
+                            'x-rapidapi-host': 'imdb8.p.rapidapi.com',
+                            'x-rapidapi-key': '61398a6ee8msh0033ca9207b7556p1fdb09jsn1ea8d45f729a'
+                        },
+                        params : {tconst: id}
+                    })
+                    .then(function (response) {
+                        setDataMovie(prevMovie => ({
+                            data : [...prevMovie.data , response.data.images[0]] 
+                        }))
+                    })
+                    .catch(function (error) { });
+                })
+            })
+            .catch()     
+        }
+         getAllImdbCode()
+    },[])
+
     return (  
         <div className='slider_slideGenreBanner'>
             <Swiper spaceBetween={0} slidesPerView={1} navigation>
-                <SwiperSlide className={Styles.sliderSlide}>
-                <img src='https://img.gs/knzwmsmxwd/741x400/https://m.media-amazon.com/images/M/MV5BOGI0OGU4NDMtNjc0Zi00ODc1LTlhNTktYTliMjAzNWEzM2JmXkEyXkFqcGdeQXVyNjczOTE0MzM@._V1_.jpg'/>
-
-                    <div className={Styles.sliderFooter}>
-                        <span>Title : Fast And Furios </span>
-                        
-                        <div className={Styles.sliderFooter_detail}>
-                            <span>Rating : 9.2</span>
-                            <span>Year : 2021</span>
-                            <span>Genre : Action</span>
-                        </div>
-                        
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide className={Styles.sliderSlide}>
-                    <img src='https://img.gs/knzwmsmxwd/741x400/https://m.media-amazon.com/images/M/MV5BNjliN2E5YWYtODUyYy00Mjg0LWI3MTctM2RmYTFhOWYzMTQ2XkEyXkFqcGdeQXVyMjkwMjE3Mjk@._V1_.jpg'/>
-                    <div className={Styles.sliderFooter}>
-                        <span>Title : Fast And Furios </span>
-                        
-                        <div className={Styles.sliderFooter_detail}>
-                            <span>Rating : 9.2</span>
-                            <span>Year : 2021</span>
-                            <span>Genre : Action</span>
-                        </div>
-                        
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide className={Styles.sliderSlide}>
-                <img src='https://img.gs/knzwmsmxwd/741x400/https://m.media-amazon.com/images/M/MV5BZjkwMjQ4MDYtNzNmYy00ODRlLWI1MTEtODFjZGE2ZjRkMmNjXkEyXkFqcGdeQXVyMjAwNDk0MDA@._V1_.jpg'/>
-                    <div className={Styles.sliderFooter}>
-                        <span>Title : Fast And Furios </span>
-                        
-                        <div className={Styles.sliderFooter_detail}>
-                            <span>Rating : 9.2</span>
-                            <span>Year : 2021</span>
-                            <span>Genre : Action</span>
-                        </div>
-                        
-                    </div>
-                </SwiperSlide>
-
+                {dataMovie.data ?.map((movie,index) => {
+                    return(
+                        <SwiperSlide className={Styles.sliderSlide}key={index}>
+                            <img src={`https://img.gs/knzwmsmxwd/741x400/${movie.url}`}/>
+                            <div className={Styles.sliderFooter}>
+                                <span>{movie.caption.length > 40 ? movie.caption.substring(0,40)+"..." : movie.caption } </span>
+                                <div className={Styles.sliderFooter_detail}>
+                                    <span>Type : {movie.relatedTitles[0].titleType}</span>
+                                    <span>Year : {movie.relatedTitles[0].year}</span>
+                                </div>
+                            </div>
+                        </SwiperSlide>  
+                    )
+                })}
             </Swiper>
         </div>
     );
