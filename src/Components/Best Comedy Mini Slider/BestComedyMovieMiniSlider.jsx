251c28e0a9fa384219a57af49fe5c9d2e3a78661
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-22 16:53:21 +0330

    Change Fetching API in => BestComedyMiniSlider Component

--- src/Components/Best Comedy Mini Slider/BestComedyMovieMiniSlider.jsx
+++ src/Components/Best Comedy Mini Slider/BestComedyMovieMiniSlider.jsx
@@ -28,31 +28,35 @@ const BestComedyMiniSlider = () => {
         // Get Imdb movie id & map on id and get Movie by id
 
         const getAllImdbCode = async() => {
-            const endPoint = "https://imdb8.p.rapidapi.com/title/get-popular-movies-by-genre"
-            await axios.get(endPoint , {params : {genre: '/chart/popular/genre/comedy'} ,   headers: {
-                'x-rapidapi-host': 'imdb8.p.rapidapi.com',
-                'x-rapidapi-key': '61398a6ee8msh0033ca9207b7556p1fdb09jsn1ea8d45f729a'
-              }})
+            const endPoint = "https://data-imdb1.p.rapidapi.com/movie/byGen/Comedy/"
+            await axios.get(endPoint ,{
+                params : {
+                    page_size: '15'
+                }, 
+                headers: {
+                    'x-rapidapi-host': 'data-imdb1.p.rapidapi.com',
+                    'x-rapidapi-key': '61398a6ee8msh0033ca9207b7556p1fdb09jsn1ea8d45f729a'
+                },
+            })
             .then(response => {
-                const  responseData =  response.data.slice(0,19)
+                const  responseData =  response.data.results;
                 responseData.map(movieId => {
-                    const id = movieId.replace("/title/",'').replace("/",'')
+                    const id = movieId.imdb_id
 
                     axios.get(`https://data-imdb1.p.rapidapi.com/movie/id/${id}/`,{headers : { 'x-rapidapi-host': 'data-imdb1.p.rapidapi.com','x-rapidapi-key': '61398a6ee8msh0033ca9207b7556p1fdb09jsn1ea8d45f729a'}})
                     .then(function (response) {
-                        setDataMovie(prevMovie => ({data : [...prevMovie.data , response.data.results] }) )
+                        setDataMovie(prevMovie => ({
+                            data : [...prevMovie.data , response.data.results] 
+                        }))
                     })
-                    .catch(function (error) {
-                        console.error(error);
-                    });
+                    .catch(function (error) { });
 
                 })
             })
             .catch()     
         }
-        // getAllImdbCode()
+        getAllImdbCode()
         
-
     },[])
 
     return (  
@@ -63,52 +67,14 @@ const BestComedyMiniSlider = () => {
             </div>
             <Swiper slidesPerView={6} spaceBetween={10} navigation freeMode={true}>
                
-                <SwiperSlide className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BNjZjNDE1NTYtYTgwZS00M2VmLWEyODktM2FlNjhiYTk3OGU2XkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg"/>
-                </SwiperSlide>
-                
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BNjE5NzA4ZDctOTJkZi00NzM0LTkwOTYtMDI4MmNkMzIxODhkXkEyXkFqcGdeQXVyNjY1MTg4Mzc@._V1_.jpg"/>
-                </SwiperSlide>
-                
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BMWRiZGQ1NDMtODQ2OS00MDlhLWJkZGYtM2ZmNjlhZThjOWRmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg"/>
-                </SwiperSlide>  
-
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BMmZiMjdlN2UtYzdiZS00YjgxLTgyZGMtYzE4ZGU5NTlkNjhhXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg"/>
-                </SwiperSlide>  
-
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BZTgxMWJkMzItMzg1YS00NDJiLTljYjctMTc2YzQzZDZjZDAyXkEyXkFqcGdeQXVyODQ2OTIzNDU@._V1_.jpg"/>
-                </SwiperSlide>    
-
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BMTlkMmVmYjktYTc2NC00ZGZjLWEyOWUtMjc2MDMwMjQwOTA5XkEyXkFqcGdeQXVyNTI4MzE4MDU@._V1_.jpg"/>
-                </SwiperSlide>   
-
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BNmQxZTNiODYtNzBhYy00MzVlLWJlN2UtNTc4YWZjMDIwMmEzXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg"/>
-                </SwiperSlide>
-
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BMTkxMjYyNzgwMl5BMl5BanBnXkFtZTgwMTE3MjYyMTE@._V1_.jpg"/>
-                </SwiperSlide>
-
-
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BNGM3YzdlOWYtNjViZS00MTE2LWE1MWUtZmE2ZTcxZjcyMmU3XkEyXkFqcGdeQXVyODEyMTI1MjA@._V1_.jpg"/>
-                </SwiperSlide>
-
-
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BNTdmMDNmYmItOWFmNC00YzdkLWIyZWMtMGRlMTQyZDZmNDU0XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg"/>
-                </SwiperSlide>
-
-
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BMTYzODYzODU2Ml5BMl5BanBnXkFtZTgwNTc1MTA2NzE@._V1_.jpg"/>
-                </SwiperSlide>
+                {dataMovie.data.length === 0 && <p style={{color:'#ffffff'}}>Loading...</p>}
+                {dataMovie.data && dataMovie.data.map(movie => {
+                    return(
+                        <SwiperSlide className={Styles.sliderSlideParent}>
+                            <img className={Styles.sliderSlide} src={`https://img.gs/knzwmsmxwd/268x215,quality=high/${movie.banner}`}/>
+                        </SwiperSlide>
+                    )
+                })}
                 
             </Swiper>
         </div>
