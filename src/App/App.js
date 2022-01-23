bcbbaef0ca9e1aabfb7b2dd186c46027ab16e0a8
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-23 20:16:01 +0330

    Connect Redux to react

--- src/App/App.js
+++ src/App/App.js
@@ -2,16 +2,21 @@
 import pageRoutes from '../Routes/routes'
 import {Route , Routes} from 'react-router-dom'
 import Layout from '../Layout/Layout';
+import { Provider } from 'react-redux';
+import store from '../Redux/store';
+
 
 function App() {
   return (
-      <Layout>
-            <Routes>
-                {pageRoutes.map(route => {return (
-                  <Route key={route.id} path={route.path}  element={route.element} />
-                ) })}
-          </Routes>    
-      </Layout>
+    <Provider store={store}>
+            <Layout>
+                    <Routes>
+                        {pageRoutes.map(route => {return (
+                          <Route key={route.id} path={route.path}  element={route.element} />
+                        ) })}
+                  </Routes>    
+              </Layout>
+      </Provider>
   );
 }
 
