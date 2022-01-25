9d04cd5c6d4e036414420194fc1c24efb67f6598
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-25 04:45:15 +0330

    Get Data with redux in Slider Component => MoviesOrderByRating Reducer

--- src/Components/Slider & Genre Banner/Slider/Slider.jsx
+++ src/Components/Slider & Genre Banner/Slider/Slider.jsx
@@ -6,50 +6,30 @@ import 'swiper/css/navigation';
 import './manageSlider.css'
 import { useEffect, useState } from 'react';
 
-import axios from 'axios'
+import {moviesOrderByRating_list} from '../../../Redux/Movies Order By Rating/MoviesOrderByRating'
+
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 const Slider = () => {
 
-    const [dataMovie , setDataMovie] = useState({data : []})
+const {data , loading , error} = useSelector(state => state.headSlider)
 
+    console.log("data : ",data)
+    console.log("loading : ",loading)
+    console.log("error : ",error)
+    console.log("------------------------------------------------------")
+    const dispatch = useDispatch()
+    
     useEffect(()=>{
-        const getAllImdbCode = async() => {
-            const endPoint = "https://data-imdb1.p.rapidapi.com/movie/order/byRating/"
-            await axios.get(endPoint ,{
-                params : { page_size: '1'}, 
-                headers: {
-                    'x-rapidapi-host': 'data-imdb1.p.rapidapi.com',
-                    'x-rapidapi-key': '61398a6ee8msh0033ca9207b7556p1fdb09jsn1ea8d45f729a'
-                },
-            })
-            .then(response => {
-                const  responseData =  response.data.results;
-                responseData.map(movieId => {
-                    const id = movieId.imdb_id
-
-                    axios.get(`https://imdb8.p.rapidapi.com/title/get-images`,{
-                        headers : {     
-                            'x-rapidapi-host': 'imdb8.p.rapidapi.com',
-                            'x-rapidapi-key': '61398a6ee8msh0033ca9207b7556p1fdb09jsn1ea8d45f729a'
-                        },
-                        params : {tconst: id}
-                    })
-                    .then(function (response) {
-                        setDataMovie(prevMovie => ({
-                            data : [...prevMovie.data , response.data.images[0]] 
-                        }))
-                    })
-                    .catch(function (error) { });
-                })
-            })
-            .catch()     
-        }
-         getAllImdbCode()
+        dispatch(moviesOrderByRating_list(2))
     },[])
 
     return (  
         <div className='slider_slideGenreBanner'>
+            {loading && <p className={Styles.loading}>Loading data...</p>}
+            {error && <p className={Styles.error}>{error}</p>}
             <Swiper spaceBetween={0} slidesPerView={1} navigation>
-                {dataMovie.data ?.map((movie,index) => {
+                {data ?.map((movie,index) => {
                     return(
                         <SwiperSlide className={Styles.sliderSlide}key={index}>
                             <img src={`https://img.gs/knzwmsmxwd/741x400/${movie.url}`}/>
