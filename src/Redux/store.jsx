4bbc4e89c1e4a2f13739f4e89ba341b8029d5a70
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-25 22:26:07 +0330

    render PopularMoviesReducer in  => store.jsx

--- src/Redux/store.jsx
+++ src/Redux/store.jsx
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import {headSliderReducer} from "./Movies Order By Rating/MoviesOrderByRating";
+import { PopularMoviesReducer } from "./Popular Movies/PopularMoviesReducer";
 import { PopularSeriesTvsReducer } from "./Popular Series Tvs/PopularSeriesTvsReducer";
 
 
@@ -7,6 +8,7 @@ const store = configureStore({
     reducer : {
         headSlider : headSliderReducer,
         popularSeriesTvs : PopularSeriesTvsReducer,
+        PopularMovies : PopularMoviesReducer
     }
 })
 export default store
