1c9b1dc864c6e7bc7f338d526958ef73116d6555
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-25 22:21:44 +0330

    Get Async Data with redux in BestMovieMiniSlider Component => PopularMovies Reducer

--- src/Components/Best Movie Mini Slider/BestMovieMiniSlider.jsx
+++ src/Components/Best Movie Mini Slider/BestMovieMiniSlider.jsx
@@ -5,58 +5,28 @@ import Styles from '../Mini Slider Style/MiniSlider.module.css'
 
 import { AiFillCaretRight } from "react-icons/ai";
 
-// Slider ==>
-
 import "swiper/css";
 import "swiper/css/free-mode"
 
 import SwiperCore, {
   FreeMode,Navigation
 } from 'swiper';
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect } from "react";
+import { getAsyncPopularMovies } from "../../Redux/Popular Movies/PopularMoviesReducer";
 
-// <==  Slider 
+import { useDispatch , useSelector} from "react-redux";
 
 SwiperCore.use([FreeMode , Navigation]);
 
-
-
 const BestMovieMiniSlider = () => {
+    const dispatch = useDispatch()
 
-    const [dataMovie , setDataMovie] = useState({data : []})
-
+    const {data , loading , error} = useSelector(state =>state.PopularMovies)
 
     useEffect(()=>{
-        const getAllImdbCode = async() => {
-            const endPoint = "https://data-imdb1.p.rapidapi.com/movie/order/byPopularity/"
-            await axios.get(endPoint ,{
-                params : {
-                    page_size: '19'
-                }, 
-                headers: {
-                    'x-rapidapi-host': 'data-imdb1.p.rapidapi.com',
-                    'x-rapidapi-key': '61398a6ee8msh0033ca9207b7556p1fdb09jsn1ea8d45f729a'
-                },
-            })
-            .then(response => {
-                const  responseData =  response.data.results;
-                responseData.map(movieId => {
-                    const id = movieId.imdb_id
-
-                    axios.get(`https://data-imdb1.p.rapidapi.com/movie/id/${id}/`,{headers : { 'x-rapidapi-host': 'data-imdb1.p.rapidapi.com','x-rapidapi-key': '61398a6ee8msh0033ca9207b7556p1fdb09jsn1ea8d45f729a'}})
-                    .then(function (response) {
-                        setDataMovie(prevMovie => ({
-                            data : [...prevMovie.data , response.data.results] 
-                        }))
-                    })
-                    .catch(function (error) { });
-                })
-            })
-            .catch()     
-        }
-        getAllImdbCode()
+        dispatch(getAsyncPopularMovies(22))
     },[])
+
     return (  
         <div className="slider_miniSlider">
             <div className={Styles.silderTitle}>
@@ -64,8 +34,9 @@ const BestMovieMiniSlider = () => {
                 <AiFillCaretRight/>
             </div>
             <Swiper slidesPerView={6} spaceBetween={10} navigation freeMode={true}>
-                {!dataMovie && <p style={{color:'#ffffff'}}>Loading...</p>}
-                {dataMovie.data && dataMovie.data.map((movie,index) => {
+                {loading && <p className={Styles.loading}>Loading...</p>}
+                {error && <p className={Styles.error}>{error}</p>}
+                {data?.map((movie,index) => {
                     return(
                         <SwiperSlide className={Styles.sliderSlideParent} key={index}>
                             <img className={Styles.sliderSlide} src={`https://img.gs/knzwmsmxwd/268x215,quality=high/${movie.banner}`}/>
