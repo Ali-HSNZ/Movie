ba58ab86c9785f38715196c257d2190a2e3c946a
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-26 03:48:50 +0330

    Get Async Data with redux in BestComedyMiniSlider Component => BestComedyMoviesReducer Reducer

--- src/Components/Best Comedy Mini Slider/BestComedyMovieMiniSlider.jsx
+++ src/Components/Best Comedy Mini Slider/BestComedyMovieMiniSlider.jsx
@@ -5,58 +5,26 @@ import Styles from '../Mini Slider Style/MiniSlider.module.css'
 
 import { AiFillCaretRight } from "react-icons/ai";
 
-// Slider ==>
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAsyncBestMoviesByComedy } from "../../Redux/Best Comedy Movies/BestComedyMoviesReducer";
+
 
 import "swiper/css";
 import "swiper/css/free-mode"
-
 import SwiperCore, {
   FreeMode,Navigation
 } from 'swiper';
-import { useEffect, useState } from "react";
-import axios from "axios";
-
-// <==  Slider 
 
 SwiperCore.use([FreeMode , Navigation]);
 
 const BestComedyMiniSlider = () => {
-    const [dataMovie , setDataMovie] = useState({data : []})
 
-    
+    const {data , loading , error} = useSelector(state =>state.BestMoviesByComedy)
+    const dispatch = useDispatch()
+
     useEffect(()=>{
-        // Get Imdb movie id & map on id and get Movie by id
-
-        const getAllImdbCode = async() => {
-            const endPoint = "https://data-imdb1.p.rapidapi.com/movie/byGen/Comedy/"
-            await axios.get(endPoint ,{
-                params : {
-                    page_size: '15'
-                }, 
-                headers: {
-                    'x-rapidapi-host': 'data-imdb1.p.rapidapi.com',
-                    'x-rapidapi-key': '61398a6ee8msh0033ca9207b7556p1fdb09jsn1ea8d45f729a'
-                },
-            })
-            .then(response => {
-                const  responseData =  response.data.results;
-                responseData.map(movieId => {
-                    const id = movieId.imdb_id
-
-                    axios.get(`https://data-imdb1.p.rapidapi.com/movie/id/${id}/`,{headers : { 'x-rapidapi-host': 'data-imdb1.p.rapidapi.com','x-rapidapi-key': '61398a6ee8msh0033ca9207b7556p1fdb09jsn1ea8d45f729a'}})
-                    .then(function (response) {
-                        setDataMovie(prevMovie => ({
-                            data : [...prevMovie.data , response.data.results] 
-                        }))
-                    })
-                    .catch(function (error) { });
-
-                })
-            })
-            .catch()     
-        }
-        getAllImdbCode()
-        
+        dispatch(getAsyncBestMoviesByComedy({byGenre : "Comedy" , count : 15}))
     },[])
 
     return (  
@@ -66,16 +34,15 @@ const BestComedyMiniSlider = () => {
                 <AiFillCaretRight/>
             </div>
             <Swiper slidesPerView={6} spaceBetween={10} navigation freeMode={true}>
-               
-                {dataMovie.data.length === 0 && <p style={{color:'#ffffff'}}>Loading...</p>}
-                {dataMovie.data?.map((movie,index) => {
+                {loading && <p className={Styles.loading}>Loading...</p>}
+                {error && <p className={Styles.error}>Loading...</p>}
+                {data?.map((movie,index) => {
                     return (
                         <SwiperSlide className={Styles.sliderSlideParent} key={index}>
-                            <img className={Styles.sliderSlide} src={`https://img.gs/knzwmsmxwd/268x215,quality=high/${movie.banner}`} />
+                            <img className={Styles.sliderSlide} src={`https://img.gs/knzwmsmxwd/268x215,quality=low/${movie.banner}`} />
                         </SwiperSlide>
                     );
                 })}
-                
             </Swiper>
         </div>
     );
