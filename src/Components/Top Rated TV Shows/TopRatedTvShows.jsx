89c0fabf305972ac3518c11aaae5f802865c921d
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-27 17:31:10 +0330

    Get And Show Async Data in => TopRatedTvShows Component

--- src/Components/Top Rated TV Shows/TopRatedTvShows.jsx
+++ src/Components/Top Rated TV Shows/TopRatedTvShows.jsx
@@ -5,8 +5,6 @@ import Styles from '../Mini Slider Style/MiniSlider.module.css'
 
 import { AiFillCaretRight } from "react-icons/ai";
 
-// Slider ==>
-
 import "swiper/css";
 import "swiper/css/free-mode"
 
@@ -14,13 +12,22 @@ import SwiperCore, {
   FreeMode,Navigation
 } from 'swiper';
 
-// <==  Slider 
+import { useDispatch } from "react-redux";
+import { getAsyncTopRatingTvShow } from "../../Redux/Top Rating Tv Show/TopRatingTvShowReducer";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 SwiperCore.use([FreeMode , Navigation]);
 
-
-
 const TopRatedTvShow = () => {
+
+    const dispatch = useDispatch()
+    const {data , loading , error} = useSelector(state => state.TopRatingTvShow)
+
+    useEffect(()=>{
+        dispatch(getAsyncTopRatingTvShow(20))
+    } , [])
+
     return (  
         <div className="slider_miniSlider">
             <div className={Styles.silderTitle}>
@@ -28,57 +35,17 @@ const TopRatedTvShow = () => {
                 <AiFillCaretRight/>
             </div>
             <Swiper slidesPerView={6} spaceBetween={10} navigation freeMode={true}>
-               
-                <SwiperSlide className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src='https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BZWYxODViMGYtMGE2ZC00ZGQ3LThhMWUtYTVkNGE3OWU4NWRkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMjYwNDA2MDE@._V1_.jpg'/>
-                </SwiperSlide>
-                
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BNmZlYzIzMTItY2EzYS00YTEyLTg0ZjEtMDMzZjM3ODdhN2UzXkEyXkFqcGdeQXVyNjI0MDg2NzE@._V1_.jpg"/>
-                </SwiperSlide>
-                
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BOWNlMjBjZTUtNThiNy00OTkxLThiZTQtNTEyZDliZTM3N2Q0XkEyXkFqcGdeQXVyMTMzNDExODE5._V1_.jpg"/>
-                </SwiperSlide>  
-
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BMTI3ODc2ODc0M15BMl5BanBnXkFtZTYwMjgzNjc3._V1_.jpg"/>
-                </SwiperSlide>  
-
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BZGQ2YmMxZmEtYjI5OS00NzlkLTlkNTEtYWMyMzkyMzc2MDU5XkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_.jpg"/>
-                </SwiperSlide>    
-
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BNTllYzFhMjAtZjExNS00MjM4LWE5YmMtOGFiZGRlOTU5YzJiXkEyXkFqcGdeQXVyNDIzMzcwNjc@._V1_.jpg"/>
-                </SwiperSlide>   
-
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BYjg2ODk0MjUtNmMzZS00MjY0LWI1YWMtN2JhMjRmZGUwY2I3XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"/>
-                </SwiperSlide>
-
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BN2I1ZjA5YjQtYmQ0ZS00ZmE1LTk1ZjktNTQ5ODIzY2JiZDdhXkEyXkFqcGdeQXVyNjg2NjQwMDQ@._V1_.jpg"/>
-                </SwiperSlide>
-
-
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BZTk5OTQyZjYtMDk3Yy00YjhmLWE2MTYtZmY4NTg1YWUzZTQ0XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg"/>
-                </SwiperSlide>
-
-
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BODc5YTBhMTItMjhkNi00ZTIxLWI0YjAtNTZmOTY0YjRlZGQ0XkEyXkFqcGdeQXVyODUwNjEzMzg@._V1_.jpg"/>
-                </SwiperSlide>
-
-
-                <SwiperSlide  className={Styles.sliderSlideParent}>
-                    <img className={Styles.sliderSlide} src="https://img.gs/knzwmsmxwd/268x215,quality=low/https://m.media-amazon.com/images/M/MV5BMTY4MGQyNjgtMzdmZS00MjQ5LWIyMzItYjYyZmQzNjVhYjMyXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg"/>
-                </SwiperSlide>
-                
+                {loading && <p className={Styles.loading}>Loading...</p>}
+                {error && <p className={Styles.error}>{error}</p>}
+                {data ?.map(movie => {
+                    return (
+                        <SwiperSlide  className={Styles.sliderSlideParent}>
+                            <img className={Styles.sliderSlide} src={`https://img.gs/knzwmsmxwd/268x215,quality=low/${movie.banner}`}/>
+                        </SwiperSlide>
+                    )
+                })}    
             </Swiper>
         </div>
     );
 }
- 
 export default TopRatedTvShow;
