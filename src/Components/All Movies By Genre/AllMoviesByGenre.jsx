70de1fd815f537b604a907f29454d7dd62928c2b
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-27 21:41:03 +0330

    Get And Show Async Data in => AllMoviesByGenr Component

--- src/Components/All Movies By Genre/AllMoviesByGenre.jsx
+++ src/Components/All Movies By Genre/AllMoviesByGenre.jsx
@@ -1,7 +1,20 @@
 import Styles from "./AllMoviesByGenre.module.css"
 import { AiFillCaretRight } from "react-icons/ai";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getAsyncAllGenres } from "../../Redux/All Movies by Genres/AllMoviesByGenres";
+import { useSelector } from "react-redux";
 
 const AllMoviesByGenre = () => {
+    
+    const dispatch = useDispatch()
+
+    useEffect(()=>{
+        dispatch(getAsyncAllGenres(9))
+    },[])
+
+    const {data , loading , error} = useSelector(state => state.AllGenres)
+
     return (  
         <div className={Styles.parent}>
             <div className={Styles.header}>
@@ -11,152 +24,36 @@ const AllMoviesByGenre = () => {
                 </p>
             </div>
             <div className={Styles.footer}>
+                {loading && <p className={Styles.loading}>Loading...</p>}
+                {error && <p className={Styles.error}>{error}</p>}
+                {data ?.map((genre,index) => {
 
-                <div className={Styles.row}>
-                    <div className={Styles.rowItem}>
-                        <div className={Styles.genre}>
-                                <section className={Styles.genreRanke_genreLine}>
-                                    <p>1</p>
-                                    <section className={Styles.genreLine}></section>
-                                </section>
-                                
-                                <p className={Styles.genreTitle}>Adventure</p>
-                                <button className={Styles.genreSubmit}>Go</button>
-                        </div>
-                        <span className={Styles.genreVidoeCount}>Video : 15</span>
-                    </div>
-
-                    <div className={Styles.rowItem}>
-                        <div className={Styles.genre}>
-                                <section className={Styles.genreRanke_genreLine}>
-                                    <p>2</p>
-                                    <section className={Styles.genreLine}></section>
-                                </section>
-                                
-                                <p className={Styles.genreTitle}>Adventure</p>
-                                <button className={Styles.genreSubmit}>Go</button>
-                        </div>
-                        <span className={Styles.genreVidoeCount}>Video : 15</span>
-                    </div>
+                    // slashOne ==>  http://47.254.174.28/movie/byGen/
+                    // slashTwo ==>  /?
+                    // genreName ===> http://47.254.174.28/movie/byGen/   <Genre Name>     /? 
+                    const slashOne = genre.next.indexOf("byGen/"); 
+                    const slashTwo = genre.next.indexOf("/?"); 
+                    const genreName = genre.next.substring(slashOne +6, slashTwo)
                     
-                    <div className={Styles.rowItem}>
-                        <div className={Styles.genre}>
+                    return(
+                        <div className={Styles.rowItem} key={index}>
+                            <div className={Styles.genre}>
                                 <section className={Styles.genreRanke_genreLine}>
-                                    <p>3</p>
+                                    <p>{index+1}</p>
                                     <section className={Styles.genreLine}></section>
                                 </section>
                                 
-                                <p className={Styles.genreTitle}>Adventure</p>
+                                <p className={Styles.genreTitle}>{genreName}</p>
                                 <button className={Styles.genreSubmit}>Go</button>
+                            </div>
+                            <span className={Styles.genreVidoeCount}>Movie : {genre.count}</span>
                         </div>
-                        <span className={Styles.genreVidoeCount}>Video : 15</span>
-                    </div>
-
-
-
-
-
-                </div>
-
-                <div className={Styles.row}>
-                    <div className={Styles.rowItem}>
-                        <div className={Styles.genre}>
-                                <section className={Styles.genreRanke_genreLine}>
-                                    <p>4</p>
-                                    <section className={Styles.genreLine}></section>
-                                </section>
-                                
-                                <p className={Styles.genreTitle}>Adventure</p>
-                                <button className={Styles.genreSubmit}>Go</button>
-                        </div>
-                        <span className={Styles.genreVidoeCount}>Video : 15</span>
-                    </div>
-
-                    <div className={Styles.rowItem}>
-                        <div className={Styles.genre}>
-                                <section className={Styles.genreRanke_genreLine}>
-                                    <p>5</p>
-                                    <section className={Styles.genreLine}></section>
-                                </section>
-                                
-                                <p className={Styles.genreTitle}>Adventure</p>
-                                <button className={Styles.genreSubmit}>Go</button>
-                        </div>
-                        <span className={Styles.genreVidoeCount}>Video : 15</span>
-                    </div>
-                    
-                    <div className={Styles.rowItem}>
-                        <div className={Styles.genre}>
-                                <section className={Styles.genreRanke_genreLine}>
-                                    <p>6</p>
-                                    <section className={Styles.genreLine}></section>
-                                </section>
-                                
-                                <p className={Styles.genreTitle}>Adventure</p>
-                                <button className={Styles.genreSubmit}>Go</button>
-                        </div>
-                        <span className={Styles.genreVidoeCount}>Video : 15</span>
-                    </div>
-
-
-
-
-
-                </div>
-
-                <div className={Styles.row}>
-                    <div className={Styles.rowItem}>
-                        <div className={Styles.genre}>
-                                <section className={Styles.genreRanke_genreLine}>
-                                    <p>7</p>
-                                    <section className={Styles.genreLine}></section>
-                                </section>
-                                
-                                <p className={Styles.genreTitle}>Adventure</p>
-                                <button className={Styles.genreSubmit}>Go</button>
-                        </div>
-                        <span className={Styles.genreVidoeCount}>Video : 15</span>
-                    </div>
-
-                    <div className={Styles.rowItem}>
-                        <div className={Styles.genre}>
-                                <section className={Styles.genreRanke_genreLine}>
-                                    <p>8</p>
-                                    <section className={Styles.genreLine}></section>
-                                </section>
-                                
-                                <p className={Styles.genreTitle}>Adventure</p>
-                                <button className={Styles.genreSubmit}>Go</button>
-                        </div>
-                        <span className={Styles.genreVidoeCount}>Video : 15</span>
-                    </div>
-                    
-                    <div className={Styles.rowItem}>
-                        <div className={Styles.genre}>
-                                <section className={Styles.genreRanke_genreLine}>
-                                    <p>9</p>
-                                    <section className={Styles.genreLine}></section>
-                                </section>
-                                
-                                <p className={Styles.genreTitle}>Adventure</p>
-                                <button className={Styles.genreSubmit}>Go</button>
-                        </div>
-                        <span className={Styles.genreVidoeCount}>Video : 15</span>
-                    </div>
-
-
-
-
-
-                </div>
-
+                    )
+                })}
             </div>
 
-            <div className={Styles.cover}>
-                <a href="/">Load More</a>
-            </div>
+            {data.length > 0 ? <div className={Styles.cover}><a href="/">Load More</a></div> : null}
         </div>
     );
 }
- 
 export default AllMoviesByGenre;
