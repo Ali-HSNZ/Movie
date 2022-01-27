6ba63057090e10782f057be9555e1a43e7a30192
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-27 22:51:35 +0330

    Get Async Data with Redux-toolkit in => BestWarMoviesReducer Reducer

---
+++ src/Redux/Best War Movies/BestWarMoviesReducer.jsx
@@ -0,0 +1,53 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getAsyncBestMoviesByWar = createAsyncThunk("Movies/BestWarMoviesAsync" , async(count ,{rejectWithValue,dispatch}) => {
+    
+
+    try {
+        const endPoint = `https://data-imdb1.p.rapidapi.com/movie/byYear/2020/byGen/War/`
+        const AsyncBestWarMovies_list = await axios.get(endPoint ,{
+            params: {page_size: count},
+            headers: {
+                'x-rapidapi-host': 'data-imdb1.p.rapidapi.com',
+                'x-rapidapi-key': '0760fbecffmsh1446d59419ae965p1f92e0jsn73eef9fcda88'
+            },
+        })
+        return dispatch(getAsyncMoviesByImdbId(AsyncBestWarMovies_list.data.results))
+    } catch (error) {
+        return rejectWithValue(error.message)
+    }
+})
+
+const getAsyncMoviesByImdbId = createAsyncThunk("Movies/BestWarMoviesAsync" , async(imdbId_list ,{rejectWithValue}) => {
+    try {
+        const AsyncPopularMovies_MovieData = await axios.all(imdbId_list.map(movieId => {
+            return  axios.get(`https://data-imdb1.p.rapidapi.com/movie/id/${movieId.imdb_id}/`,{
+                headers : {
+                    'x-rapidapi-host': 'data-imdb1.p.rapidapi.com',
+                    'x-rapidapi-key': '61398a6ee8msh0033ca9207b7556p1fdb09jsn1ea8d45f729a'
+                }
+            })
+        }))
+       return AsyncPopularMovies_MovieData.map(e => e.data.results)
+    } catch (error) {
+        return rejectWithValue(error.message)
+    }
+})
+
+const BestWarMovies = createSlice({
+    name : "BestWarMovies",
+    initialState : {data : [] , loading : false , error : null},
+    extraReducers : {
+        [getAsyncBestMoviesByWar.pending] : ( state , action ) => {
+            return {data : [] , error : null , loading : true}
+        },
+        [getAsyncBestMoviesByWar.fulfilled] : ( state , action ) => {
+            return {data : action.payload.payload , error : null , loading : false}
+        },
+        [getAsyncBestMoviesByWar.rejected] : ( state , action ) => {
+            return {data : [] , error : action.payload , loading : false}
+        },
+    },
+})
+export const BestMoviesByWarReducer = BestWarMovies.reducer;
