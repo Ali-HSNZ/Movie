53483828edd7ab97d80c25c9711b8dae42838740
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-28 04:36:54 +0330

    Change/ Change Async Actions Names & Reducer Name in => AllMoviesByGenres Reducer

--- src/Redux/All Movies by Genres/AllMoviesByGenres.jsx
+++ src/Redux/All Movies by Genres/AllMoviesByGenres.jsx
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const getAsyncAllGenres = createAsyncThunk("Movies/AllGenresAsync" , async(count ,{rejectWithValue,dispatch}) => {
+export const getAsyncAllMoviesByGenre = createAsyncThunk("Movies/AllMoviesByGenre" , async(count ,{rejectWithValue,dispatch}) => {
     try {
         const endPoint = "https://data-imdb1.p.rapidapi.com/genres/"
         const AsyncAllGenres_list = await axios.get(endPoint ,{
@@ -10,16 +10,15 @@ export const getAsyncAllGenres = createAsyncThunk("Movies/AllGenresAsync" , asyn
                 'x-rapidapi-key': 'ef9d3c3f13msh2474c99e5fa56d7p1fa5c1jsn3babf0ae016f'
             },
         })
-        return dispatch(getAsyncAllMoviesByGenres(AsyncAllGenres_list.data.results.slice(0,count)))
+        dispatch(getAsyncAllMovies(AsyncAllGenres_list.data.results.slice(0,count)))
     } catch (error) {
         return rejectWithValue(error.message)
     }
 })
 
-const getAsyncAllMoviesByGenres = createAsyncThunk("Movies/AllMoviesByGenre" , async(dataList ,{rejectWithValue}) => {
+const getAsyncAllMovies = createAsyncThunk("Movies/AllMoviesByGenre" , async(dataList ,{rejectWithValue}) => {
     
     const genreList = dataList.map(e => e.genre)
-
     try {
         const AsyncGetMoviesByGenres = await axios.all(genreList.map(genre => {
             return  axios.get(`https://data-imdb1.p.rapidapi.com/movie/byGen/${genre}/`,{
@@ -29,26 +28,25 @@ const getAsyncAllMoviesByGenres = createAsyncThunk("Movies/AllMoviesByGenre" , a
                 }
             })
         }))
-
-       return AsyncGetMoviesByGenres.map(e => e.data)
+        return AsyncGetMoviesByGenres.map(e => e.data)
     } catch (error) {
         return rejectWithValue(error.message)
     }
 })
 
-const AllGenres = createSlice({
-    name : "AllGenres",
+const AllMoviesByGenre = createSlice({
+    name : "AllMoviesByGenres",
     initialState : {data : [] , loading : false , error : null},
     extraReducers : {
-        [getAsyncAllGenres.pending] : ( state , action ) => {
+        [getAsyncAllMoviesByGenre.pending] : ( state , action ) => {
             return {data : [] , error : null , loading : true}
         },
-        [getAsyncAllGenres.fulfilled] : ( state , action ) => {
-            return {data : action.payload.payload , error : null , loading : false}
+        [getAsyncAllMoviesByGenre.fulfilled] : ( state , action ) => {
+            return {data : action.payload , error : null , loading : false}
         },
-        [getAsyncAllGenres.rejected] : ( state , action ) => {
+        [getAsyncAllMoviesByGenre.rejected] : ( state , action ) => {
             return {data : [] , error : action.payload , loading : false}
         },
     },
 })
-export const AllGenresReducer = AllGenres.reducer;
+export const AllMoviesByGenreReducer = AllMoviesByGenre.reducer;
