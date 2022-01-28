4b7282661bd67c551de074db50e292c27f92a79d
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-29 03:10:37 +0330

    use Skeleton Component Material-UI in => BestWarMovieMiniSlider Component

--- src/Components/Best War Movies Mini Slider/BestWarMoviesMiniSlider.jsx
+++ src/Components/Best War Movies Mini Slider/BestWarMoviesMiniSlider.jsx
@@ -16,6 +16,7 @@ import SwiperCore, {
   FreeMode,Navigation
 } from 'swiper';
 import MiniSliderSlideCommon from "../../Common/Mini Slider Slide/MiniSliderSlide";
+import { Skeleton } from "@mui/material";
 
 SwiperCore.use([FreeMode , Navigation]);
 
@@ -23,9 +24,24 @@ const BestWarMovieMiniSlider = () => {
     const {data , error , loading} = useSelector(state => state.BestMoviesByWar)
     const dispatch = useDispatch()
 
+    const NumOfvideos = 18;
     useEffect(()=>{
-        dispatch(getAsyncBestMoviesByWar(20))
+        dispatch(getAsyncBestMoviesByWar(NumOfvideos))
     },[])
+
+    const renderSkeleton = ()=>{
+        let content = [];
+        for (let index = 0; index < NumOfvideos; index++) {
+            content.push(
+                <SwiperSlide>
+                    <Skeleton  variant="rectangular" width={215} height={268} sx={{ bgcolor: "#1d1d2e" }}/>
+                    <Skeleton  variant="rectangular" width={100} height={15} sx={{ bgcolor: "#181824" }}/>
+                    <Skeleton  variant="rectangular" width={215} height={18} sx={{ bgcolor: "#1d1d2e" }}/>
+                </SwiperSlide>   
+            );
+        }
+        return content
+    }
 
     return (  
         <div className="slider_miniSlider">
@@ -34,7 +50,7 @@ const BestWarMovieMiniSlider = () => {
                 <AiFillCaretRight/>
             </div>
             <Swiper slidesPerView={6} spaceBetween={10} navigation freeMode={true}>
-                {loading && <p className={Styles.loading}>Loading...</p>}
+                {loading && renderSkeleton()}
                 {error && <p className={Styles.error}>{error}</p>}
                 {data ? data.map((movie,index) => {
                     return(
@@ -42,7 +58,7 @@ const BestWarMovieMiniSlider = () => {
                             <MiniSliderSlideCommon movie={movie}/>
                         </SwiperSlide>
                     )
-                }) : <p className={Styles.loading}>Loading...</p>}
+                }) : renderSkeleton()}
             </Swiper>
         </div>
     );
