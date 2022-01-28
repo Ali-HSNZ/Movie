52727a4d890120f4069a716184f6941e2e34489c
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-29 03:12:53 +0330

    use Skeleton Component Material-UI in => BestFamilyMovieMiniSlider Component

--- src/Components/Best Family Movie Mini Slider/BestFamilyMovieMiniSlider.jsx
+++ src/Components/Best Family Movie Mini Slider/BestFamilyMovieMiniSlider.jsx
@@ -13,11 +13,11 @@ import "swiper/css/free-mode"
 import SwiperCore, {
   FreeMode,Navigation
 } from 'swiper';
-import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getAsyncBestMoviesByFamily } from "../../Redux/Best Family Movies/BestFamilyMovies";
 import MiniSliderSlideCommon from "../../Common/Mini Slider Slide/MiniSliderSlide";
+import { Skeleton } from "@mui/material";
 
 SwiperCore.use([FreeMode , Navigation]);
 
@@ -27,9 +27,24 @@ const BestFamilyMovieMiniSlider = () => {
     const dispatch = useDispatch()
     const {data , loading , error} = useSelector(state => state.BestMoviesByFamily)
 
+    const NumOfvideos = 18;
     useEffect(()=>{
-        dispatch(getAsyncBestMoviesByFamily(20))
+        dispatch(getAsyncBestMoviesByFamily(NumOfvideos))
     },[])
+
+    const renderSkeleton = ()=>{
+        let content = [];
+        for (let index = 0; index < NumOfvideos; index++) {
+            content.push(
+                <SwiperSlide>
+                    <Skeleton  variant="rectangular" width={215} height={268} sx={{ bgcolor: "#1d1d2e" }}/>
+                    <Skeleton  variant="rectangular" width={100} height={15} sx={{ bgcolor: "#181824" }}/>
+                    <Skeleton  variant="rectangular" width={215} height={18} sx={{ bgcolor: "#1d1d2e" }}/>
+                </SwiperSlide>   
+            );
+        }
+        return content
+    }
 
     return (  
         <div className="slider_miniSlider">
@@ -38,7 +53,7 @@ const BestFamilyMovieMiniSlider = () => {
                 <AiFillCaretRight/>
             </div>
             <Swiper slidesPerView={6} spaceBetween={10} navigation freeMode={true}>
-                {loading && <p className={Styles.loading}>Loading...</p>}
+                {loading && renderSkeleton()}
                 {error && <p className={Styles.error}>{error}</p>}
                 {data ? data.map((movie,index) => {
                     return (
@@ -46,7 +61,7 @@ const BestFamilyMovieMiniSlider = () => {
                             <MiniSliderSlideCommon movie={movie}/>
                         </SwiperSlide>
                     )
-                 }) : <p className={Styles.loading}>Loading...</p>}
+                 }) : renderSkeleton()}
             </Swiper>
         </div>
     );
