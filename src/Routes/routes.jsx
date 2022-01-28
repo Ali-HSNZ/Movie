31cc9a0ae07d375cb2b20aee84efc65164e02383
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-28 23:26:07 +0330

    Render Movie Page in path: '/movie/id:?'

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -1,6 +1,8 @@
 import Home from "../Pages/Home/Home"
+import MoviePage from "../Pages/Movie/Movie"
 
  const pageRoutes = [
     {id : 1 , path : "/" , element : <Home/> },
+    {id : 2 , path : "/movie/id:id" , element : <MoviePage/> },
 ]
 export default pageRoutes
