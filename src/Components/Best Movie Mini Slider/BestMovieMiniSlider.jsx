06cee7ad63867af56fb359d889c21ca06b09a3e0
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-28 04:22:49 +0330

    Added Loading in BestMovieMiniSlider Component

--- src/Components/Best Movie Mini Slider/BestMovieMiniSlider.jsx
+++ src/Components/Best Movie Mini Slider/BestMovieMiniSlider.jsx
@@ -36,13 +36,13 @@ const BestMovieMiniSlider = () => {
             <Swiper slidesPerView={6} spaceBetween={10} navigation freeMode={true}>
                 {loading && <p className={Styles.loading}>Loading...</p>}
                 {error && <p className={Styles.error}>{error}</p>}
-                {data?.map((movie,index) => {
+                {data?data.map((movie,index) => {
                     return(
                         <SwiperSlide className={Styles.sliderSlideParent} key={index}>
                             <img className={Styles.sliderSlide} src={`https://img.gs/knzwmsmxwd/268x215,quality=high/${movie.banner}`}/>
                         </SwiperSlide>
                     )
-                })}
+                }) : <p className={Styles.loading}>Loading...</p>}
             </Swiper>
         </div>
     );
