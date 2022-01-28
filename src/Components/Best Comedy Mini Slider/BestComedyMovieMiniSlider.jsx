07d73734e31db8c32acf7058e0c42d7d49427f55
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-28 22:21:25 +0330

    Render MiniSliderSlideCommon Component in => BestComedyMiniSlider Component

--- src/Components/Best Comedy Mini Slider/BestComedyMovieMiniSlider.jsx
+++ src/Components/Best Comedy Mini Slider/BestComedyMovieMiniSlider.jsx
@@ -9,6 +9,8 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAsyncBestMoviesByComedy } from "../../Redux/Best Comedy Movies/BestComedyMoviesReducer";
 
+import MiniSliderSlideCommon from "../../Common/Mini Slider Slide/MiniSliderSlide";
+
 
 import "swiper/css";
 import "swiper/css/free-mode"
@@ -38,8 +40,8 @@ const BestComedyMiniSlider = () => {
                 {error && <p className={Styles.error}>{error}</p>}
                 {data ? data.map((movie,index) => {
                     return (
-                        <SwiperSlide className={Styles.sliderSlideParent} key={index}>
-                            <img className={Styles.sliderSlide} src={`https://img.gs/knzwmsmxwd/268x215,quality=low/${movie.banner}`} />
+                        <SwiperSlide key={index}>
+                            <MiniSliderSlideCommon movie={movie}/>
                         </SwiperSlide>
                     );
                 }) : <p className={Styles.loading}>Loading...</p>}
