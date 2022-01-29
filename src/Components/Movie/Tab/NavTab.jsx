056cdb62dfdf1036861dfbc771a81233c4c1d4d0
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-29 22:24:57 +0330

    Create Routes & Link in NavTab Component

--- src/Components/Movie/Tab/NavTab.jsx
+++ src/Components/Movie/Tab/NavTab.jsx
@@ -1,21 +1,45 @@
 import Styles from './NavTab.module.css'
-import Box from '@mui/material/Box';
-import { Link, Route, Routes } from 'react-router-dom';
-import { Button } from '@mui/material';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import { Skeleton } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import {getAsyncMovieDataWithImdbId} from '../../../Redux/Get Movie Data With imdb Id/GetMovieDataWithImdbId'
+import {useQuery} from '../../../hooks/useQuery'
+import {Main , Actor , ProductionLocations , AllNews , Awards} from '../Components';
+import { useEffect } from 'react';
 
-function NavTabs() {
+
+function NavTabs(props) {
+
+    const query = useQuery().get("id")
+
+    const {data , loading , error} = useSelector(state => state.MovieDataWithImdbId)
+
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        dispatch(getAsyncMovieDataWithImdbId(query))
+    },[query])
+
+
     return (
         <>
-            <Box sx={{ width: '100%' }}>
-                <Button className={Styles.Button}><Link to=""> Main</Link></Button>
-                <Button className={Styles.Button}><Link to="actors">Actors</Link></Button>
-                <Button className={Styles.Button}><Link to="productionLocations">Production locations</Link></Button>
-                <Button className={Styles.Button}><Link to="awards">awards</Link></Button>
-                <Button className={Styles.Button}><Link to="allNews">All News</Link></Button>
-            </Box>
+        {loading && <Skeleton variant='rectangular' width={500} height={27}  sx={{ bgcolor: "#1d1d2e" }} style={{margin : "20px 0"}}/>}
+        {error && <p className={Styles.error}>{error}</p>}
+        {data && <h1 className={Styles.movieTitle}>{data.title}</h1>}
+            
+            <div className={Styles.tabParent}>
+               <NavLink className={({isActive}) => isActive ? Styles.tabActive : Styles.tabNotActive} to={{pathname:"main" , search:`id=${query}`}}> Main</NavLink>
+               <NavLink className={({isActive}) => isActive ? Styles.tabActive : Styles.tabNotActive}  to={{pathname:"actors" , search:`id=${query}`}}>Actors</NavLink>
+               <NavLink className={({isActive}) => isActive ? Styles.tabActive : Styles.tabNotActive}  to={{pathname:"production-locations" , search:`id=${query}`}}>Production locations</NavLink>
+               <NavLink className={({isActive}) => isActive ? Styles.tabActive : Styles.tabNotActive}  to={{pathname:"awards" , search:`id=${query}`}}>Awards</NavLink>
+               <NavLink className={({isActive}) => isActive ? Styles.tabActive : Styles.tabNotActive}  to={{pathname:"all-news" , search:`id=${query}`}}>All News</NavLink>
+            </div>
 
             <Routes>
-                <Route path=""></Route>
+                <Route path={`/main`} element={<Main/>}></Route>
+                <Route path={`/actors`} element={<Actor/>}></Route>
+                <Route path={`/production-locations`} element={<ProductionLocations/>}></Route>
+                <Route path={`/awards`} element={<Awards/>}></Route>
+                <Route path={`/all-news`} element={< AllNews/>}></Route>
             </Routes>
             
         </>
