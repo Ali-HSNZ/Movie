d33f27825e3ff1e476e3ac5b3a3820fab221ded0
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-01-30 18:42:05 +0330

    Create Serial Page Route

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -1,8 +1,10 @@
 import Home from "../Pages/Home/Home"
 import MoviePage from "../Pages/Movie/Movie"
+import SerialPage from "../Pages/Serial/Serial"
 
  const pageRoutes = [
     {id : 1 , path : "/" , element : <Home/> },
     {id : 2 , path : "/movie/*" , element : <MoviePage/> },
+    {id : 3 , path : "/serial/*" , element : <SerialPage/> },
 ]
 export default pageRoutes
