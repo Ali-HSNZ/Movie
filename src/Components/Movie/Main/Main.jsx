279217006027b5dd0abf23a3857b2e1d361b4479
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-02-05 18:06:17 +0330

    Create News Component in Main Component & Get Async News

--- src/Components/Movie/Main/Main.jsx
+++ src/Components/Movie/Main/Main.jsx
@@ -3,8 +3,24 @@ import { useDispatch, useSelector } from "react-redux"
 import { useQuery } from "../../../hooks/useQuery"
 import { getAsyncMovieDataWithImdbId } from "../../../Redux/Get Movie Data With imdb Id/GetMovieDataWithImdbId"
 import { useEffect, useState } from "react"
-import { Skeleton} from "@mui/material"
+import { makeStyles} from "@mui/styles"
+import { PaginationItem, Skeleton} from "@mui/material"
+
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+
+import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
+import ArrowForwardRoundedIcon from '@mui/icons-material/ArrowForwardRounded';
+
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 import { getAsyncSynopsesDataWithImdbId } from "../../../Redux/Get Movie Synopses With Imdb Id/GetMovieSynopsesWithImdbId"
+import { getAsyncNewsDataWithImdbId } from "../../../Redux/Get Movie News By Imdb Id/GetMovieNewsByImdbId"
 
 const Main = () => {
     const query = useQuery().get("id")
@@ -13,12 +29,14 @@ const Main = () => {
     useEffect(()=>{
         dispatch(getAsyncMovieDataWithImdbId(query))
         dispatch(getAsyncSynopsesDataWithImdbId(query))
+        dispatch(getAsyncNewsDataWithImdbId(query))
     },[query])
 
     return (  
         <div className={Styles.parent}>
             <MovieDetail/>
             <Synopses/>
+            <News/>
         </div>
     )
 }
@@ -134,6 +152,139 @@ export const Synopses = () => {
                     ) }
                 </>
             )}
+        </div>
+    );
+}
+
+
+export const News = () => {
+
+    const { newsData , newsError ,newsLoading } = useSelector(state => state.MovieNewsWithImdbId)
+
+    const [newsPageNum , setNewsPageNum] = useState(0)
+    const numOfNewsOnPage = 6;
+    const newsReaded = newsPageNum * numOfNewsOnPage;
+    const showNews = newsData.slice(newsReaded , newsReaded + numOfNewsOnPage)
+    .map(item => {
+        return (
+            <Accordion>
+            <AccordionSummary
+                expandIcon={<ExpandMoreIcon sx={{color:"#fff"}}/>}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+                sx={{bgcolor:"#1c1c2b" , color:"#cccccc"}}
+                >
+                    <Typography>{item.head}</Typography>
+            </AccordionSummary>
+            <AccordionDetails 
+                sx={{
+                    bgcolor:"#222236" ,
+                     color:"#cccccc",
+    
+                }}
+            >
+                <Typography sx={{fontWeight:"100",fontSize:"14px" , fontFamily:"Open Sans light", lineHeight:"26px"}}>{item.body}</Typography>
+                
+            </AccordionDetails>
+        </Accordion>
+        )
+    })
+    const pageCount = newsData ? Math.ceil(newsData.length / numOfNewsOnPage) : 0
+
+
+    const useStyles = makeStyles((theme) =>({
+        root: {
+            backgroundColor: 'transparent',
+            color:'#181824',
+            display:'flex',
+            justifyContent:'center',
+            marginTop:'20px',
+            "& .MuiPaginationItem-previousNext" :{
+                backgroundColor:'#0e0e17',
+                color:"#A8B60C",
+                padding : "17px 25px",
+                border:"none"
+            },
+            "&  button":{
+                border:"1px solid #000",
+                color:"#A8B60C",
+            },
+            "& div":{
+                color:"#A8B60C",
+            },
+            "& .Mui-selected":{
+                backgroundColor:'#A8B60C',
+                color:"black"
+            }
+        },
+    }));
+    const classes = useStyles();
+
+
+    return (  
+        <div className={Styles.movieNewsParent}>
+
+        {newsError && <p className={Styles.error}>{newsError}</p>}
+        {newsData && newsData.length > 0  ? (
+            <>
+                <h1 className={Styles.movieNewsTitle}>News</h1>
+                <section className={Styles.movieItem}>
+                    {newsData && showNews }
+                </section>
+
+                <Stack spacing={5}>
+                    <Pagination 
+                        count={pageCount}  
+                        shape={"rounded"} 
+                        variant={"string"} 
+                        sx= {{
+                            "&& .Mui-selected": {
+                                backgroundColor: "#d4e60e"
+                            },
+                            "&& .Mui-selected:hover": {
+                                backgroundColor: "#a8b330"
+                            }
+                        }}
+                        onChange={(event , pageNum) =>  setNewsPageNum(pageNum-1)}
+                        classes={{root : classes.root}}    
+                        renderItem={(item) => (
+                            <PaginationItem
+                                components={{ previous: ArrowBackRoundedIcon , next: ArrowForwardRoundedIcon}}
+                                {...item}
+                            />
+                        )}
+                    />
+                </Stack>
+            </>
+        ) : (
+        <>
+            {newsLoading && (
+                <>
+                    <Skeleton variant='rectangular' width={187} height={33} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#1d1d2e"}}/>
+                    <Skeleton variant='rectangular' width={187} height={13} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#181824" , minWidth : "100%" }}/>   
+
+                    <Skeleton variant='rectangular' width={187} height={25} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#1d1d2e"  , minWidth : "100%"}}/>
+                    <Skeleton variant='rectangular' width={187} height={13} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#181824" , minWidth : "100%" }}/>   
+
+                    <Skeleton variant='rectangular' width={187} height={25} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#1d1d2e"  , minWidth : "100%"}}/>
+                    <Skeleton variant='rectangular' width={187} height={13} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#181824" , minWidth : "100%" }}/>   
+
+                    <Skeleton variant='rectangular' width={187} height={25} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#1d1d2e"  , minWidth : "100%"}}/>
+                    <Skeleton variant='rectangular' width={187} height={13} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#181824" , minWidth : "100%" }}/>   
+
+                    <Skeleton variant='rectangular' width={187} height={25} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#1d1d2e"  , minWidth : "100%"}}/>
+                    <Skeleton variant='rectangular' width={187} height={13} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#181824" , minWidth : "100%" }}/>   
+
+                    <Skeleton variant='rectangular' width={187} height={25} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#1d1d2e"  , minWidth : "100%"}}/>
+                    <Skeleton variant='rectangular' width={187} height={13} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#181824" , minWidth : "100%" }}/>   
+                    
+                    <Skeleton variant='rectangular' width={187} height={25} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#1d1d2e"  , minWidth : "100%"}}/>
+                    <Skeleton variant='rectangular' width={187} height={13} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#181824" , minWidth : "100%" }}/>   
+                </>
+            )}
+        </>
+        )}
+            
         </div>
     );
 }
