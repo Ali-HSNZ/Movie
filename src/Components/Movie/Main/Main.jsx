1331858bb2693e2e792d09ebc0314a9b830d0d14
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-02-05 13:23:15 +0330

    Create & Render MovieDetail Component in Movie Component

--- src/Components/Movie/Main/Main.jsx
+++ src/Components/Movie/Main/Main.jsx
@@ -3,24 +3,33 @@ import { useDispatch, useSelector } from "react-redux"
 import { useQuery } from "../../../hooks/useQuery"
 import { getAsyncMovieDataWithImdbId } from "../../../Redux/Get Movie Data With imdb Id/GetMovieDataWithImdbId"
 import { useEffect } from "react"
-import { Skeleton } from "@mui/material"
+import { Skeleton} from "@mui/material"
 
 const Main = () => {
     const query = useQuery().get("id")
-
-    const { data } = useSelector(state => state.MovieDataWithImdbId)
-
-    const movie_hr = data && Math.floor(data.movie_length/60); 
-    const movie_min = data && Math.floor(data.movie_length % 60);
 
     const dispatch = useDispatch()
     useEffect(()=>{
         dispatch(getAsyncMovieDataWithImdbId(query))
     },[query])
 
-
     return (  
         <div className={Styles.parent}>
+            <MovieDetail/>
+        </div>
+    )
+}
+export default Main;
+
+export const MovieDetail = () => {
+
+    const { data } = useSelector(state => state.MovieDataWithImdbId)
+
+    const movie_hr = data && Math.floor(data.movie_length/60); 
+    const movie_min = data && Math.floor(data.movie_length % 60);
+
+    return (  
+        <header className={Styles.header}>
             <div className={Styles.imgParent}>
                 {data.banner ? (
                     <img src={`https://img.gs/knzwmsmxwd/187x280,quality=high/${data.banner}`}/>
@@ -29,17 +38,17 @@ const Main = () => {
                 )}
             </div>
             <div className={Styles.describtionParent}>
-            {data.description ? (
-                <h4>{data.description}</h4>
-            ) : (
-                <>
-                    <Skeleton variant='rectangular' width={187} height={22} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#1d1d2e" , minWidth : "100%" }}/>
-                    <Skeleton variant='rectangular' width={187} height={13} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#181824" , minWidth : "100%" }}/>
-                    <Skeleton variant='rectangular' width={187} height={22} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#1d1d2e" , minWidth : "100%" }}/>
-                    <Skeleton variant='rectangular' width={187} height={13} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#181824" , minWidth : "100%" }}/>
-                    <Skeleton variant='rectangular' width={187} height={22} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#1d1d2e" , minWidth : "100%" }}/>
-                </>
-            )}
+                {data.description ? (
+                    <h4>{data.description}</h4>
+                ) : (
+                    <>
+                        <Skeleton variant='rectangular' width={187} height={22} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#1d1d2e" , minWidth : "100%" }}/>
+                        <Skeleton variant='rectangular' width={187} height={13} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#181824" , minWidth : "100%" }}/>
+                        <Skeleton variant='rectangular' width={187} height={22} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#1d1d2e" , minWidth : "100%" }}/>
+                        <Skeleton variant='rectangular' width={187} height={13} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#181824" , minWidth : "100%" }}/>
+                        <Skeleton variant='rectangular' width={187} height={22} className = {Styles.describtionParent_keleton} sx={{ bgcolor: "#1d1d2e" , minWidth : "100%" }}/>
+                    </>
+                )}
             </div>
             <div className={Styles.propertiesParent}>
                 <div className={Styles.propertiesGroup}>
@@ -79,8 +88,6 @@ const Main = () => {
                 </div>
 
             </div>
-        </div>
-    )
+        </header>
+    );
 }
- 
-export default Main;
