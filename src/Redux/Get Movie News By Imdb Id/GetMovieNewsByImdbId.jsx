089f1704a586d1b4c4b52184854f9988f046ca94
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-02-05 18:09:14 +0330

    Create GetMovieNewsByImdbId Reducer

---
+++ src/Redux/Get Movie News By Imdb Id/GetMovieNewsByImdbId.jsx
@@ -0,0 +1,37 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { imdb8_apiKey } from "../../Services/API_KEY";
+
+export const  getAsyncNewsDataWithImdbId = createAsyncThunk("Movies/GetMovieNewsWithImdbId" , async(movieId ,{rejectWithValue}) => {    
+    try {
+        const movieData = await axios.get(`https://imdb8.p.rapidapi.com/title/get-news`,{
+            params: {tconst: `${movieId}` , limit: '50'},
+            headers : {
+                'x-rapidapi-host': 'imdb8.p.rapidapi.com',
+                'x-rapidapi-key': imdb8_apiKey
+            }
+        })
+        return movieData.data.items
+
+    } catch (error) {
+        return rejectWithValue(error.message)
+    }
+})
+
+const MovieNewsWithImdbId = createSlice({
+    name : "MovieNewsWithImdbId",
+    initialState : {newsData : [] , newsLoading : false , newsError : null},
+    extraReducers : {
+        [getAsyncNewsDataWithImdbId.pending] : ( state , action ) => {
+            return {newsData : [] , newsError : null , newsLoading : true} 
+        },
+        [getAsyncNewsDataWithImdbId.fulfilled] : ( state , action ) => {
+            console.log("action.payload : ",action.payload)
+            return {newsData : action.payload , newsError : null , newsLoading : false}
+        },
+        [getAsyncNewsDataWithImdbId.rejected] : ( state , action ) => {
+            return {newsData : [] , newsError : action.payload , newsLoading : false}
+        },
+    },
+})
+export const MovieNewsWithImdbIdReducer = MovieNewsWithImdbId.reducer;
