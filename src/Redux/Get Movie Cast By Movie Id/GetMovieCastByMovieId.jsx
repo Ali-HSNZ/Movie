d77621c2ac739a20ee1715020c2b4ab9571c71c0
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-02-05 23:48:09 +0330

    Create GetMovieCastByMovieId Reducer To Get Async Movie Casts

---
+++ src/Redux/Get Movie Cast By Movie Id/GetMovieCastByMovieId.jsx
@@ -0,0 +1,54 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getAsyncGetMovieCastByImdbId = createAsyncThunk("Movies/GetMovieCastsByImdbId" , async(movieId ,{rejectWithValue,dispatch}) => {
+    console.log(movieId)
+    try {
+        const endPoint = `https://data-imdb1.p.rapidapi.com/movie/id/${movieId}/cast/`
+        const AsyncMovieCasts_list = await axios.get(endPoint ,{
+            headers: {
+                'x-rapidapi-host': 'data-imdb1.p.rapidapi.com',
+                'x-rapidapi-key': '423451439cmsh37b65307257e1d8p1f2fdajsn7ec4491007b3'
+            },
+        })
+        const AsyncMovieCastsData = AsyncMovieCasts_list.data.results.roles.length > 20 ? AsyncMovieCasts_list.data.results.roles.slice(0,20) : AsyncMovieCasts_list.data.results.roles
+        dispatch(getAsyncCastBio(AsyncMovieCastsData))
+    } catch (error) {
+        return rejectWithValue(error.message)
+    }
+})
+
+const getAsyncCastBio = createAsyncThunk("Movies/GetMovieCastsByImdbId" , async(imdbId_list ,{rejectWithValue}) => {
+    console.log(imdbId_list)
+    try {
+        const AsyncActoreBio = await axios.all(imdbId_list.map(cast => {
+            return  axios.get(`https://data-imdb1.p.rapidapi.com/actor/id/${cast.actor.imdb_id}/`,{
+                headers : {
+                    'x-rapidapi-host': 'data-imdb1.p.rapidapi.com',
+                    'x-rapidapi-key': '423451439cmsh37b65307257e1d8p1f2fdajsn7ec4491007b3'
+                }
+            })
+        }))
+       return AsyncActoreBio.map(e => e.data.results)
+    } catch (error) {
+        return rejectWithValue(error.message)
+    }
+})
+
+const MovieCasts = createSlice({
+    name : "MovieCasts",
+    initialState : {castData : [] , castLoading : false , castError : null},
+    extraReducers : {
+        [getAsyncGetMovieCastByImdbId.pending] : ( state , action ) => {
+            return {castData : [] , castError : null , castLoading : true}
+        },
+        [getAsyncGetMovieCastByImdbId.fulfilled] : ( state , action ) => {
+            console.log("action.payload < ",action.payload)
+            return {castData : action.payload , castError : null , castLoading : false}
+        },
+        [getAsyncGetMovieCastByImdbId.rejected] : ( state , action ) => {
+            return {castData : [] , castError : action.payload , castLoading : false}
+        },
+    },
+})
+export const MovieCastsReducer = MovieCasts.reducer;
