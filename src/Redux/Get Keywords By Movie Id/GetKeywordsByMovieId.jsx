ea105d9e808028bcfaab18429ed79d264cf6e4a5
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-02-06 18:57:41 +0330

    Create GetKeywordsByMovieId Reducer

---
+++ src/Redux/Get Keywords By Movie Id/GetKeywordsByMovieId.jsx
@@ -0,0 +1,33 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const  getAsyncMovieKeywordsWithMovieId = createAsyncThunk("Movies/getAsyncMovieKeywordsWithMovieId" , async(movieId ,{rejectWithValue}) => {    
+    try {
+        const movieKeywords = await axios.get(`https://data-imdb1.p.rapidapi.com/movie/id/${movieId}/keywords/`,{
+            headers : {
+                'x-rapidapi-host': 'data-imdb1.p.rapidapi.com',
+                'x-rapidapi-key': '396322dc0fmsh453a908c695847cp158f3cjsn36782745f6c3'
+            }
+        })
+        return movieKeywords.data.results
+    } catch (error) {
+        return rejectWithValue(error.message)
+    }
+})
+
+const MovieKeywordsWithMovieId = createSlice({
+    name : "MovieKeywordsWithMovieId",
+    initialState : {data : [] , loading : false , error : null},
+    extraReducers : {
+        [getAsyncMovieKeywordsWithMovieId.pending] : ( state , action ) => {
+            return {data : [] , error : null , loading : true}
+        },
+        [getAsyncMovieKeywordsWithMovieId.fulfilled] : ( state , action ) => {
+            return {data : action.payload , error : null , loading : false}
+        },
+        [getAsyncMovieKeywordsWithMovieId.rejected] : ( state , action ) => {
+            return {data : [] , error : action.payload , loading : false}
+        },
+    },
+})
+export const MovieKeywordsWithMovieIdReducer = MovieKeywordsWithMovieId.reducer;
