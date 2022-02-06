bc6a3be0df3909fb151d4b1d60e1da8441a40bf2
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-02-06 18:53:55 +0330

    Render Keywords Component in Movie Main Component

--- src/Components/Movie/Main/Main.jsx
+++ src/Components/Movie/Main/Main.jsx
@@ -2,13 +2,15 @@ import MovieDetail from "./Movie Detail/MovieDetail"
 import News from "./Movie News/News"
 import Casts from "./Movie Casts/Casts"
 import Synopses from "./Movie Synopses/Synopses"
+import Keywords from './Movie Keywords/Keywords'
 const Main = () => {
     return (  
         <>
             <MovieDetail/>
             <Synopses/>
-            <News/>
             <Casts/>
+            <News/>
+            <Keywords/>
         </>
     )
 }
