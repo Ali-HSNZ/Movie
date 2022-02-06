d0702c0ac10082fcfec464f0bb08be5de67fbc31
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-02-06 22:33:39 +0330

    If The ID Changes, Scroll Up in => Movie Main Component

--- src/Components/Movie/Main/Main.jsx
+++ src/Components/Movie/Main/Main.jsx
@@ -3,7 +3,18 @@ import News from "./Movie News/News"
 import Casts from "./Movie Casts/Casts"
 import Synopses from "./Movie Synopses/Synopses"
 import Keywords from './Movie Keywords/Keywords'
+import MoreLikeThis from './Movie More Like This/MoreLikeThis'
+import { useQuery } from "../../../hooks/useQuery"
+import { useEffect } from "react"
+
 const Main = () => {
+    
+    const query = useQuery().get("id")
+
+    useEffect(()=>{
+        window.scroll({top:0 ,behavior:"smooth"})
+    },[query])
+    
     return (  
         <>
             <MovieDetail/>
@@ -11,6 +22,7 @@ const Main = () => {
             <Casts/>
             <News/>
             <Keywords/>
+            <MoreLikeThis/>
         </>
     )
 }
