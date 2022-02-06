08f798562c0c44a22d66da01261f9a1d98691299
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-02-06 22:30:23 +0330

    Dispatch Action/ Get One Coming Soon Movie in => Slider Component

--- src/Components/Home/Slider & Genre Banner/Slider/Slider.jsx
+++ src/Components/Home/Slider & Genre Banner/Slider/Slider.jsx
@@ -18,7 +18,7 @@ const {data , loading , error} = useSelector(state => state.comingSoonMovies)
     const dispatch = useDispatch()
     
     useEffect(()=>{
-        dispatch(getAsyncComingSoonMovies(2))
+        dispatch(getAsyncComingSoonMovies(1))
     },[])
 
     return (  
