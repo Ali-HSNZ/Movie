29cd71b30812527ffe4c0a12ae6707f25878bf05
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-06-22 23:55:44 +0430

    removed unUsed import

--- src/Components/Movie/Movie News/News.jsx
+++ src/Components/Movie/Movie News/News.jsx
@@ -12,7 +12,6 @@ import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
-import { AiFillCaretRight } from 'react-icons/ai';
 import { useQuery } from '../../../hooks/useQuery';
 import { getAsyncNewsDataWithImdbId } from '../../../Redux/Get Movie News By Imdb Id/GetMovieNewsByImdbId';
 
