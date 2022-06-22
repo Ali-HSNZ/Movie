252308f22bfef057450ab133d9926f22c7ff16ef
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-06-22 23:45:19 +0430

    Added many Skeleton & Arrange Code - Cast Component

--- src/Components/Movie/Movie Casts/Casts.jsx
+++ src/Components/Movie/Movie Casts/Casts.jsx
@@ -2,7 +2,7 @@ import Styles from './Casts.module.css'
 import userImage from '../../../images/userImage.png'
 import { Skeleton} from "@mui/material"
 import { useDispatch, useSelector } from 'react-redux'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { getAsyncGetMovieCastByImdbId } from '../../../Redux/Get Movie Cast By Movie Id/GetMovieCastByMovieId'
 import { useQuery } from '../../../hooks/useQuery'
 
@@ -17,7 +17,7 @@ const Casts = () => {
     },[query])
 
     const {castData , castLoading , castError} = useSelector(state => state.MovieCasts)
-
+    
     return(
         <div className={Styles.parent}>
             {castError && <p className={Styles.error}>{castError}</p>}
@@ -27,7 +27,6 @@ const Casts = () => {
                         <h1 className={Styles.movieCastTitle}>Casts :</h1>
                     </header>
              
-                                   
                     <div className={Styles.footer}>
                         {castData.map((cast,index) => {
                             return(
@@ -40,8 +39,9 @@ const Casts = () => {
                                     )}
                                     </div>
                                     <hr/>
-                                    <div className={Styles.title}>
-                                        <p>{cast.name}</p>
+                                    <div  className={Styles.title}>
+                                        <p className={Styles.cast_name}>{cast.name}</p>
+                                        <p className={Styles.cast_starSign}>{cast.star_sign == "" || cast.star_sign == "-" ? 'Unknown' : cast.star_sign}</p>
                                     </div>
                                 </section>
                             )
@@ -50,7 +50,7 @@ const Casts = () => {
                 </>
             ) : (
                 <>
-                    {castLoading && (
+                    {castLoading  || castData == undefined &&(
                         <>                        
                             <Skeleton variant='rectangular' width={187} height={33} sx={{ bgcolor: "#1d1d2e"}}/>
                             <Skeleton variant='rectangular' width={187} height={13} sx={{ bgcolor: "#181824" , minWidth : "100%" }}/>   
@@ -73,8 +73,30 @@ const Casts = () => {
 
                                 <Skeleton variant='rectangular' height={25} sx={{ bgcolor: "#1d1d2e"  , minWidth : "200px"}}/>
                                 <Skeleton variant='rectangular' height={25} sx={{ bgcolor: "#181824"  , minWidth : "10px"}}/>
-
                             </div>
+
+                            <Skeleton variant='rectangular' width={187} height={13} sx={{ bgcolor: "#181824" , minWidth : "100%" }}/>   
+                            
+                            <div style={{display:'flex',flexWrap:'wrap'}}>
+                                <Skeleton variant='rectangular' height={25} sx={{ bgcolor: "#1d1d2e"  , minWidth : "200px"}}/>
+                                <Skeleton variant='rectangular' height={25} sx={{ bgcolor: "#181824"  , minWidth : "10px"}}/>
+                                
+                                <Skeleton variant='rectangular' height={25} sx={{ bgcolor: "#1d1d2e"  , minWidth : "200px"}}/>
+                                <Skeleton variant='rectangular' height={25} sx={{ bgcolor: "#181824"  , minWidth : "10px"}}/>
+
+                                <Skeleton variant='rectangular' height={25} sx={{ bgcolor: "#1d1d2e"  , minWidth : "200px"}}/>
+                                <Skeleton variant='rectangular' height={25} sx={{ bgcolor: "#181824"  , minWidth : "10px"}}/>
+
+                                <Skeleton variant='rectangular' height={25} sx={{ bgcolor: "#1d1d2e"  , minWidth : "200px"}}/>
+                                <Skeleton variant='rectangular' height={25} sx={{ bgcolor: "#181824"  , minWidth : "10px"}}/>
+
+                                <Skeleton variant='rectangular' height={25} sx={{ bgcolor: "#1d1d2e"  , minWidth : "200px"}}/>
+                                <Skeleton variant='rectangular' height={25} sx={{ bgcolor: "#181824"  , minWidth : "10px"}}/>
+
+                                <Skeleton variant='rectangular' height={25} sx={{ bgcolor: "#1d1d2e"  , minWidth : "200px"}}/>
+                                <Skeleton variant='rectangular' height={25} sx={{ bgcolor: "#181824"  , minWidth : "10px"}}/>
+                            </div>
+                            
                         </>
                     )}
                 </>
