1352635ceac6b4e5cbb38b047c08009419e45393
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-06-22 23:34:39 +0430

    create genreList Array & Arrange Code - GenreBanner Component

--- src/Components/Home/Slider & Genre Banner/GenreBanner/GenreBanner.jsx
+++ src/Components/Home/Slider & Genre Banner/GenreBanner/GenreBanner.jsx
@@ -1,30 +1,29 @@
 import Styles from './GenreBanner.module.css'
+
+const genreList = [
+    {id : 1 , imageSrc : "https://1.bp.blogspot.com/-2qBa46d-Ah0/YOXXf2Vm9gI/AAAAAAAABdc/mR9q76a9_UgzYw7uQxrrGjwd7fhATD-TwCLcBGAsYHQ/s1920/Occupation%2BRainfall%2B%2528Signature%2BEntertainment%2529%2BBanner.jpg" , genre : "Action" },
+    {id : 2 , imageSrc : "https://wallpapercave.com/wp/wp11029115.jpg" , genre : "Comedy" },
+    {id : 3 , imageSrc : "https://firsthand.co/_next/image?url=https%3A%2F%2Fmedia2.vault.com%2F16976%2Fcoco.jpg&w=640&q=75" , genre : "Animation" },
+]
+
 const GenreBanner = () => {
     return (  
-        <div className={Styles.genreBanner}>
+        <div className={Styles.parent}>
 
-        <div className={Styles.parentDiv}>
-            <div className={Styles.parentDivChildren}>
-                <div><p>Action</p></div>
-                
-            </div>    
+            {genreList.map(image => {
+                return(
+                    <div key={image.id} className={Styles.genre}  style={{
+                        background: `url('${image.imageSrc}')`,
+                        backgroundPosition:'0px -30px',
+                        backgroundSize:'',
+                        backgroundRepeat:'no-repeat'
+                    }}>
+                        <div className={Styles.popUp}></div>
+                    </div>
+                )
+            })}
+
         </div>
-
-        <div className={Styles.parentDiv}>
-            <div className={Styles.parentDivChildren}>
-                
-                <div><p>Animation</p></div>
-            </div>    
-        </div>
-
-        <div className={Styles.parentDiv}>
-            <div className={Styles.parentDivChildren}>
-                
-                <div><p>drama </p></div>
-            </div>    
-        </div>
-
-    </div>
     );
 }
  
