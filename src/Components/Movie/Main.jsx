f8ef332563858597b4b10797560e412855c5443b
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-06-24 10:08:48 +0430

    change/ change Cast import name - Movie/ Main Component

--- src/Components/Movie/Main.jsx
+++ src/Components/Movie/Main.jsx
@@ -1,6 +1,6 @@
 import MovieDetail from "./Movie Detail/MovieDetail"
 import News from "./Movie News/News"
-import Casts from "./Movie Casts/Casts"
+import Casts from "./casts/Casts"
 import Synopses from "./Movie Synopses/Synopses"
 import Keywords from './Movie Keywords/Keywords'
 import MoreLikeThis from './Movie More Like This/MoreLikeThis'
