141303a9a69715a6dc1f30090b6fd01f06119e9a
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-06-24 10:52:38 +0430

    change Action Request/ used Movie & Serial request With One Async action - Redux/ GetKeywordsByMovieId

--- src/Redux/Get Keywords By Movie Id/GetKeywordsByMovieId.jsx
+++ src/Redux/Get Keywords By Movie Id/GetKeywordsByMovieId.jsx
@@ -1,15 +1,29 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const  getAsyncMovieKeywordsWithMovieId = createAsyncThunk("Movies/getAsyncMovieKeywordsWithMovieId" , async(movieId ,{rejectWithValue}) => {    
+export const  getAsyncMovieKeywordsWithMovieId = createAsyncThunk("Movies/getAsyncMovieKeywordsWithMovieId" , async(data ,{rejectWithValue}) => {    
+    
+    const {page , query} = data
+
     try {
-        const movieKeywords = await axios.get(`https://data-imdb1.p.rapidapi.com/movie/id/${movieId}/keywords/`,{
-            headers : {
-                'x-rapidapi-host': 'data-imdb1.p.rapidapi.com',
-                'x-rapidapi-key': '396322dc0fmsh453a908c695847cp158f3cjsn36782745f6c3'
-            }
-        })
-        return movieKeywords.data.results
+        if(page === 'movie'){
+            const movieKeywords = await axios.get(`https://data-imdb1.p.rapidapi.com/movie/id/${query}/keywords/`,{
+                headers : {
+                    'x-rapidapi-host': 'data-imdb1.p.rapidapi.com',
+                    'x-rapidapi-key': '396322dc0fmsh453a908c695847cp158f3cjsn36782745f6c3'
+                }
+            })
+            return movieKeywords.data.results
+        }
+        else if(page === 'serial'){
+            const movieKeywords = await axios.get(`https://data-imdb1.p.rapidapi.com/series/id/${query}/keywords/`,{
+                headers : {
+                    'x-rapidapi-host': 'data-imdb1.p.rapidapi.com',
+                    'x-rapidapi-key': '396322dc0fmsh453a908c695847cp158f3cjsn36782745f6c3'
+                }
+            })
+            return movieKeywords.data.results
+        }
     } catch (error) {
         return rejectWithValue(error.message)
     }
