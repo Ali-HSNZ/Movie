8417db9d00240138c9a842c635873d3d1ebf1c2c
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-06-24 09:50:13 +0430

    change/ change Serial route - routes.jsx

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -1,10 +1,9 @@
 import Home from "../Pages/Home/Home"
 import MoviePage from "../Pages/Movie/Movie"
-import SerialPage from "../Pages/Serial/Serial"
 
  const pageRoutes = [
     {id : 1 , path : "/" , element : <Home/> },
     {id : 2 , path : "/movie/" , element : <MoviePage/> },
-    {id : 3 , path : "/serial/*" , element : <SerialPage/> },
+    {id : 3 , path : "/serial/" , element : <MoviePage/> },
 ]
 export default pageRoutes
