708397915116e0dd6489485b072449c22223f8fb
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-06-24 09:45:41 +0430

    added movie id dependency to useEffect for Change page Scroll

--- src/Components/Movie/Main.jsx
+++ src/Components/Movie/Main.jsx
@@ -5,12 +5,15 @@ import Synopses from "./Movie Synopses/Synopses"
 import Keywords from './Movie Keywords/Keywords'
 import MoreLikeThis from './Movie More Like This/MoreLikeThis'
 import { useEffect } from "react"
+import { useQuery } from "../../hooks/useQuery"
 
 const Main = () => {
 
+    const id = useQuery().get('id')
+
     useEffect(()=>{
         window.scroll({top:0 ,behavior:"smooth"})
-    },[])
+    },[id])
     
     return (  
         <>
