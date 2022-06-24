55e671c74a585b67fafc82a4385d4cd5f479d1c3
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-06-24 10:22:15 +0430

    change/ change Keywords import name - Movie/ Main Component

--- src/Components/Movie/Main.jsx
+++ src/Components/Movie/Main.jsx
@@ -2,7 +2,7 @@ import MovieDetail from "./detail/MovieDetail"
 import News from "./Movie News/News"
 import Casts from "./casts/Casts"
 import Synopses from "./Movie Synopses/Synopses"
-import Keywords from './Movie Keywords/Keywords'
+import Keywords from './keywords/Keywords'
 import MoreLikeThis from './Movie More Like This/MoreLikeThis'
 import { useEffect } from "react"
 import { useQuery } from "../../hooks/useQuery"
