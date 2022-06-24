0d03e76a1c77821609238da60c6c482ae288b606
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-06-24 08:52:43 +0430

    Removed unUsed Code - Movie/ Main Component

--- src/Components/Movie/Main.jsx
+++ src/Components/Movie/Main.jsx
@@ -4,16 +4,13 @@ import Casts from "./Movie Casts/Casts"
 import Synopses from "./Movie Synopses/Synopses"
 import Keywords from './Movie Keywords/Keywords'
 import MoreLikeThis from './Movie More Like This/MoreLikeThis'
-import { useQuery } from "../../hooks/useQuery"
 import { useEffect } from "react"
 
 const Main = () => {
-    
-    const query = useQuery().get("id")
 
     useEffect(()=>{
         window.scroll({top:0 ,behavior:"smooth"})
-    },[query])
+    },[])
     
     return (  
         <>
