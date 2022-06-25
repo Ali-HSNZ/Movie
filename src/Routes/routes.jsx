bdaaad3ca5d6a521fa65cb0d2ff88248d0d101e1
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-06-25 23:05:58 +0430

    Added genre Route - Routes.jsx

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -1,9 +1,11 @@
 import Home from "../Pages/Home/Home"
 import MoviePage from "../Pages/Movie/Movie"
+import Genre from './../Components/Genre/Gnere';
 
  const pageRoutes = [
     {id : 1 , path : "/" , element : <Home/> },
     {id : 2 , path : "/movie/" , element : <MoviePage/> },
     {id : 3 , path : "/serial/" , element : <MoviePage/> },
+    {id : 4 , path : '/genre/:q' , element : <Genre/>},
 ]
 export default pageRoutes
