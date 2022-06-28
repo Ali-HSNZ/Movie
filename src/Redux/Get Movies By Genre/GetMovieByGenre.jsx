e94d48581ee8d8bc72e156c96f05a84d50b23454
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-06-28 15:03:47 +0430

    Change API Request - GetMovieByGenre (Redux)

--- src/Redux/Get Movies By Genre/GetMovieByGenre.jsx
+++ src/Redux/Get Movies By Genre/GetMovieByGenre.jsx
@@ -7,15 +7,15 @@ export const getAsyncBestMoviesByGenre = createAsyncThunk("Movies/BestGenreMovie
     const {genre , count} = data;
 
     try {
-        const endPoint = `https://online-movie-database.p.rapidapi.com/title/v2/get-popular-movies-by-genre`
-        const AsyncBestGenreMovies_list = await axios.get(endPoint ,{
-            params: {genre: genre.toLowerCase(), limit: count},
+        const endPoint = `https://data-imdb1.p.rapidapi.com/movie/byYear/2020/byGen/${genre}/`
+        const AsyncBestComedyMovies_list = await axios.get(endPoint ,{
+            params: {page_size: count},
             headers: {
-                'X-RapidAPI-Key': `${imdb8_apiKey}`,
-                'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
-              }
+                'x-rapidapi-host': 'data-imdb1.p.rapidapi.com',
+                'x-rapidapi-key': '0760fbecffmsh1446d59419ae965p1f92e0jsn73eef9fcda88'
+            },
         })
-        dispatch(getAsyncMoviesByImdbId(AsyncBestGenreMovies_list.data))
+        dispatch(getAsyncMoviesByImdbId(AsyncBestComedyMovies_list.data.results))
     } catch (error) {
         return rejectWithValue(error.message)
     }
@@ -24,7 +24,7 @@ export const getAsyncBestMoviesByGenre = createAsyncThunk("Movies/BestGenreMovie
 const getAsyncMoviesByImdbId = createAsyncThunk("Movies/BestGenreMoviesAsync" , async(imdbId_list ,{rejectWithValue}) => {
     try {
         const AsyncPopularMovies_MovieData = await axios.all(imdbId_list.map(movie => {
-            const movieId = movie.replace("/title/",'').replace('/','');
+            const movieId = movie.imdb_id.replace("/title/",'').replace('/','');
             // console.log("movieId : ",movieId)
 
             return axios.get(`https://data-imdb1.p.rapidapi.com/movie/id/${movieId}/`,{
