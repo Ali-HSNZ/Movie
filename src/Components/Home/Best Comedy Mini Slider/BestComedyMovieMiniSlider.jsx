62cd105e51d9314c04163f35cf7ab55aff09ca78
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-06-28 13:57:31 +0430

    Arrange Code & change Request to get Comedy Movies - 'use getAsyncBestMoviesByGenre Request' - BestComedyMovieMiniSlider Component

--- src/Components/Home/Best Comedy Mini Slider/BestComedyMovieMiniSlider.jsx
+++ src/Components/Home/Best Comedy Mini Slider/BestComedyMovieMiniSlider.jsx
@@ -5,28 +5,29 @@ import { movieCount } from './../miniSliderSettings';
 import { AiFillCaretRight } from "react-icons/ai";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getAsyncBestMoviesByComedy } from "../../../Redux/Best Comedy Movies/BestComedyMoviesReducer";
 import MiniSliderSlideCommon from "../../../Common/Mini Slider Slide/MiniSliderSlide";
 import "swiper/css";
 import "swiper/css/free-mode"
 import SwiperCore, {FreeMode,Navigation} from 'swiper';
 import { Skeleton } from "@mui/material";
+import { Link } from 'react-router-dom';
+import { getAsyncBestMoviesByGenre } from './../../../Redux/Get Movies By Genre/GetMovieByGenre';
 
 SwiperCore.use([FreeMode , Navigation]);
 
 const BestComedyMiniSlider = () => {
 
-    const {data , loading , error} = useSelector(state =>state.BestMoviesByComedy)
+    const {data , loading , error} = useSelector(state => state.BestMoviesByGenre);
+
     const dispatch = useDispatch()
 
-    const NumOfvideos = movieCount;
     useEffect(()=>{
-        dispatch(getAsyncBestMoviesByComedy({byGenre : "Comedy" , count : NumOfvideos}))
+        dispatch(getAsyncBestMoviesByGenre({genre : "Comedy" , count : movieCount}))
     },[])
 
     const renderSkeleton = ()=>{
         let content = [];
-        for (let index = 0; index < NumOfvideos; index++) {
+        for (let index = 0; index < movieCount; index++) {
             content.push(
                 <SwiperSlide key={index}>
                     <Skeleton  variant="rectangular" width={215} height={268} sx={{ bgcolor: "#1d1d2e" }}/>
@@ -41,10 +42,10 @@ const BestComedyMiniSlider = () => {
 
     return (  
         <div className="slider_miniSlider">
-            <div className={Styles.silderTitle}>
-                <a href="/">Best Comedy Movies</a>
+            <nav className={Styles.silderTitle}>
+                <Link to="/genre/Comedy">Best Comedy Movies</Link>
                 <AiFillCaretRight/>
-            </div>
+            </nav>
             <Swiper slidesPerView={6} spaceBetween={10} navigation freeMode={true}>
                 {loading && renderSkeleton()}
                 {error && <p className={Styles.error}>{error}</p>}
