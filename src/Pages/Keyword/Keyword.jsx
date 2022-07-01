3c34afd0386c49d165563d669bdb980dc1343466
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-07-02 00:19:30 +0430

    Render Movie and Series Component - Pages/Keyword Keyword Component

--- src/Pages/Keyword/Keyword.jsx
+++ src/Pages/Keyword/Keyword.jsx
@@ -1,9 +1,23 @@
+import Movie from "./Movie/Movie";
+import Series from "./Series/Series";
 import { useQuery } from './../../hooks/useQuery';
+import Styles from './Keyword.module.css'
+import { useEffect } from 'react';
+
 const Keyword = () => {
-    const keywordName = useQuery().get('name')
+    const keyword = useQuery().get('name')
+
+    useEffect(() => {
+        window.scroll({top : 0 , behavior : 'smooth'})
+    } , [keyword])
+    
     return ( 
-        <h1>Keyword : {keywordName}</h1>
-     );
+        <>
+            {keyword && <h1 className={Styles.pageTitle}>{keyword}</h1>}
+            <Movie/>
+            <Series/>
+        </>    
+    );
 }
  
 export default Keyword;
