2c99e43644ea306a3991a57cd0b4291586ba08a2
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-07-01 19:49:43 +0430

    Create Keyword Route in Route.jsxe

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -6,6 +6,7 @@ import NotFoundPage from './../Pages/404';
 import TopRatingMovies from "../Pages/Top Rating Movies/TopRatingMovies";
 import AllSeriesByGenre from "../Pages/All Series By Genre/AllSeriesByGenre";
 import Cast from './../Pages/Cast/Cast';
+import Keyword from './../Pages/Keyword/Keyword';
 
  const pageRoutes = [
     {id : 1 , path : "/" , element : <Home/>  },
@@ -16,6 +17,7 @@ import Cast from './../Pages/Cast/Cast';
     {id : 6 , path : '/topRatingMovies' , element : <TopRatingMovies/> },
     {id : 7 , path : '/allSeriesByGenre' , element : <AllSeriesByGenre/> },
     {id : 8 , path : '/cast' , element : <Cast/> },
-    {id : 9 , path : '*', element:<NotFoundPage/>}
+    {id : 9 , path : '/Keyword', element:<Keyword/>},
+    {id : 10 , path : '*', element:<NotFoundPage/>},
 ]
 export default pageRoutes
