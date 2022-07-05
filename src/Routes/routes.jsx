b6b981fd0b91b8cce277eba31f63436c2aaf17a9
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-07-05 22:42:39 +0430

    Render AllMoviesByGenre in '/allMoviesByGenre' Route - Routes

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -7,6 +7,7 @@ import TopRatingMovies from "../Pages/Top Rating Movies/TopRatingMovies";
 import AllSeriesByGenre from "../Pages/All Series By Genre/AllSeriesByGenre";
 import Cast from './../Pages/Cast/Cast';
 import Keyword from './../Pages/Keyword/Keyword';
+import AllMoviesByGenre from "../Pages/All Movies By Genre/AllMoviesByGenre";
 
  const pageRoutes = [
     {id : 1 , path : "/" , element : <Home/>  },
@@ -15,9 +16,10 @@ import Keyword from './../Pages/Keyword/Keyword';
     {id : 4 , path : '/genre/:q' , element : <Genre/> },
     {id : 5 , path : '/popular' , element : <Popular/> },
     {id : 6 , path : '/topRatingMovies' , element : <TopRatingMovies/> },
-    {id : 7 , path : '/allSeriesByGenre' , element : <AllSeriesByGenre/> },
-    {id : 8 , path : '/cast' , element : <Cast/> },
-    {id : 9 , path : '/Keyword', element:<Keyword/>},
-    {id : 10 , path : '*', element:<NotFoundPage/>},
+    {id : 7 , path : '/allMoviesByGenre' , element : <AllMoviesByGenre/> },
+    {id : 8 , path : '/allSeriesByGenre' , element : <AllSeriesByGenre/> },
+    {id : 9 , path : '/cast' , element : <Cast/> },
+    {id : 10 , path : '/Keyword', element:<Keyword/>},
+    {id : 11 , path : '*', element:<NotFoundPage/>},
 ]
 export default pageRoutes
